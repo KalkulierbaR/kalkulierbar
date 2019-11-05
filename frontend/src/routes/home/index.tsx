9cadff0f2e8d7f860e3aa205e198f28292fe5502
drodt <d.drodt@gmx.de>
2019-11-05 17:59:35 +0100

    Remove paragraph

--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -6,7 +6,6 @@ const Home: preact.FunctionalComponent<Props> = props => {
     return (
         <div class={style.home}>
             <h1>Home</h1>
-            <p>This is the Home component.</p>
         </div>
     );
 };
