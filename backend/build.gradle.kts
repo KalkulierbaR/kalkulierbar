0e23532d6bd052aac3eaa4585e1366b7c9a9f564
rec0de <mail@rec0de.net>
2019-11-07 17:07:10 +0100

    Basic gradle build infrastructure

---
+++ backend/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.3.50"
+    application
+    eclipse
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-junit"))
+    compile("io.javalin:javalin:3.6.0")
+    compile("org.slf4j:slf4j-simple:1.8.0-beta4")
+}
+
+application {
+    mainClassName = "main.kotlin.MainKt"
+}
+
+eclipse {
+    classpath {
+        containers("org.jetbrains.kotlin.core.KOTLIN_CONTAINER")
+    }
+}
