29b1a054158b3cfe7ad3e4810257062755410cee
rec0de <mail@rec0de.net>
2019-11-07 18:26:19 +0100

    Add documentation

--- backend/src/main/kotlin/main.kt
+++ backend/src/main/kotlin/main.kt
@@ -3,22 +3,30 @@ package main.kotlin
 import io.javalin.Javalin
 import kalkulierbar.*
 
+// List of all active calculi (calculuus?)
 val endpoints: Set<Calculus> = setOf<Calculus>(ClauseAcceptor())
 
+/**
+ * Starts a Javalin Server and creates API methods for active calculus objects
+ */
 fun main(args: Array<String>) {
 
 	val port = 7000
     val app = Javalin.create().start(port)
 
+	// Serve a small overview at the root endpoint listing all active calculus identifiers
     app.get("/") { ctx -> ctx.result("KalkulierbaR API Server\n\nAvailable calculus endpoints:\n${endpoints.map{it.identifier}.joinToString("\n")}") }
 	
+	// Create API methods for each calculus
 	for(endpoint in endpoints) {
 		val name = endpoint.identifier
 		
+		// Small documentation at the main calculus endpoint
 		app.get("/$name") {
 			ctx -> ctx.result("Calculus \"$name\" loaded.\nInteract via the /parse /move and /close endpoints\n\nCalculus Documentation:\n\n${endpoint.getDocumentation()}")
 		}
 
+		// Parse endpoint takes formula parameter and passes it to calculus implementation
 		app.post("/$name/parse") { ctx ->
 			val formula = ctx.formParam("formula")
 			if(formula == null)
@@ -26,6 +34,7 @@ fun main(args: Array<String>) {
 			ctx.result(endpoint.parseFormula(formula))
 		}
 		
+		// Move endpoint takes state and move parameter values and passes them to calculus implementation
 		app.post("/$name/move") { ctx ->
 			val state = ctx.formParam("state")
 			val move = ctx.formParam("move")
@@ -36,6 +45,7 @@ fun main(args: Array<String>) {
 			ctx.result(endpoint.applyMove(state, move))
 		}
 		
+		// Close endpoint takes state parameter value and passes it to calculus implementation
 		app.post("/$name/close") { ctx ->
 			val state = ctx.formParam("state")
 			if(state == null)
