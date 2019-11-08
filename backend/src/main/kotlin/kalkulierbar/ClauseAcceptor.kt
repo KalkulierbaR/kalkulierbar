6f9226b8bd12029e00ad973c10cd7a8c2ff82c52
rec0de <mail@rec0de.net>
2019-11-08 15:59:39 +0100

    Use less ambiguous clause set format

--- backend/src/main/kotlin/kalkulierbar/ClauseAcceptor.kt
+++ backend/src/main/kotlin/kalkulierbar/ClauseAcceptor.kt
@@ -6,7 +6,7 @@ class ClauseAcceptor: Calculus {
 	/**
 	 * Parses a set of clauses from text into an example state representation
 	 * Note that the used format for both the formula and the state may differ for different implementations
-	 * @param formula set of clauses of logical variables, format: a,b|!b,c|d,!e,!f where variables are [a-zA-Z]+
+	 * @param formula set of clauses of logical variables, format: a,b;!b,c;d,!e,!f where variables are [a-zA-Z]+
 	 * @return serialized state representation of the input formula
 	 */
 	override fun parseFormula(formula: String): String {
@@ -15,13 +15,13 @@ class ClauseAcceptor: Calculus {
 		// The code could technically deal with weirder variable names, but let's keep things simple here
 		// (!)?[a-zA-Z]+ matches a single variable that may be negated, let's abbreviate that with "v"
 		// v(,v)* matches arbitrarily long lists of variables, e.g. a,b,!c,d. Let's call that "l"
-		// l(\|l)* matches arbitrarily many lists, e.g. a,b|c,!d,|e
+		// l(;l)* matches arbitrarily many lists, e.g. a,b;c,!d;e
 		// Easy, right?
-		if(!(Regex("(!)?[a-zA-Z]+(,(!)?[a-zA-Z]+)*(\\|(!)?[a-zA-Z]+(,(!)?[a-zA-Z]+)*)*") matches formula))
-			throw InvalidFormulaFormat("Invalid input formula format. Please adhere to the following format: a,b|!b,c|d,!e,!f with variables in [a-zA-Z]+")
+		if(!(Regex("(!)?[a-zA-Z]+(,(!)?[a-zA-Z]+)*(;(!)?[a-zA-Z]+(,(!)?[a-zA-Z]+)*)*") matches formula))
+			throw InvalidFormulaFormat("Invalid input formula format. Please adhere to the following format: a,b;!b,c;d,!e,!f with variables in [a-zA-Z]+")
 		
 		var parsed: MutableSet<Set<Pair<String, Boolean>>> = HashSet()
-		val clauses = formula.split("|")
+		val clauses = formula.split(";")
 		
 		for(clause in clauses) {
 			val members = clause.split(",")
@@ -68,7 +68,7 @@ class ClauseAcceptor: Calculus {
 	 */
 	override fun getDocumentation(): String {
 		val doc = "Simple test calculus that accepts sets of clauses\n" +
-				"move and close endpoints are non-functional, parse endpoint takes a set of clauses (format: a,b|!a,c|d for (a v b) ^ (!a v c) ^ (d)) and returns a state representation"
+				"move and close endpoints are non-functional, parse endpoint takes a set of clauses (format: a,b;!a,c;d for (a v b) ^ (!a v c) ^ (d)) and returns a state representation"
 		return doc
 	}
 }
