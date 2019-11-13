effc98714641bb82a3bcd87f5c1a999d760a0892
drodt <d.drodt@gmx.de>
2019-11-13 14:17:11 +0100

    Add icon to header

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -4,6 +4,11 @@ import * as style from "./style.css";
 const Header: preact.FunctionalComponent = () => {
     return (
         <header class={style.header}>
+            <img
+                class={style.logo}
+                src="/assets/icons/logo-plain.svg"
+                alt="KalkulierbaR logo"
+            />
             <h1>KalkulierbaR</h1>
         </header>
     );
