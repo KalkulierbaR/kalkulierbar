a2db3ace56c151876abff217f4922ef661c85ad4
drodt <d.drodt@gmx.de>
2019-11-15 20:25:18 +0100

    Use Elvis operator

--- backend/src/main/kotlin/main.kt
+++ backend/src/main/kotlin/main.kt
@@ -65,27 +65,23 @@ fun httpApi(port: Int, endpoints: Set<Calculus>) {
         // Parse endpoint takes formula parameter and passes it to calculus implementation
         app.post("/$name/parse") { ctx ->
             val formula = ctx.formParam("formula")
-            if (formula == null)
-                throw ApiMisuseException("POST parameter 'formula' needs to be present")
+                    ?: throw ApiMisuseException("POST parameter 'formula' needs to be present")
             ctx.result(endpoint.parseFormula(formula))
         }
 
         // Move endpoint takes state and move parameter values and passes them to calculus implementation
         app.post("/$name/move") { ctx ->
             val state = ctx.formParam("state")
+                    ?: throw ApiMisuseException("POST parameter 'state' with state representation needs to be present")
             val move = ctx.formParam("move")
-            if (state == null)
-                throw ApiMisuseException("POST parameter 'state' with state representation needs to be present")
-            if (move == null)
-                throw ApiMisuseException("POST parameter 'move' with move representation needs to be present")
+                    ?: throw ApiMisuseException("POST parameter 'move' with move representation needs to be present")
             ctx.result(endpoint.applyMove(state, move))
         }
 
         // Close endpoint takes state parameter value and passes it to calculus implementation
         app.post("/$name/close") { ctx ->
             val state = ctx.formParam("state")
-            if (state == null)
-                throw ApiMisuseException("POST parameter 'state' with state representation must be present")
+                    ?: throw ApiMisuseException("POST parameter 'state' with state representation must be present")
             ctx.result(if (endpoint.checkClose(state)) "Proof closed" else "Incomplete Proof")
         }
     }
