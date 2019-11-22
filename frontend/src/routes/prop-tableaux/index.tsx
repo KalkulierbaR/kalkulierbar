e193aa6a8b47bd1ccbf7c480b1d24463f96ab29f
drodt <d.drodt@gmx.de>
2019-11-22 15:22:33 +0100

    Change TableauxState according to backend

--- frontend/src/routes/prop-tableaux/index.tsx
+++ frontend/src/routes/prop-tableaux/index.tsx
@@ -3,17 +3,11 @@ import * as style from "./style.css";
 
 import ClauseInput from "../../components/input/clause";
 import { AppStateUpdater } from "../../types/app";
-import { TableauxState } from "../../types/tableaux";
 
 interface Props {
     server: string;
     onChange: AppStateUpdater<"prop-tableaux">;
 }
-
-const EMPTY_STATE: TableauxState = {
-    clauseSet: { clauses: [] },
-    tree: {}
-};
 
 const Tableaux: preact.FunctionalComponent<Props> = ({ server, onChange }) => {
     return (
--- frontend/src/types/tableaux.ts
+++ frontend/src/types/tableaux.ts
@@ -1,8 +1,17 @@
 import { ClauseSet } from "./clause";
 
-export interface Tree {}
+export interface TableauxNode {
+    parent: number;
+    spelling: string;
+    negated: boolean;
+    isClosed: boolean;
+    closeRef: number | null;
+    children: number[];
+}
 
 export interface TableauxState {
+    idCounter: number;
+    seal: string;
     clauseSet: ClauseSet;
-    tree: Tree;
+    nodes: TableauxNode[];
 }
