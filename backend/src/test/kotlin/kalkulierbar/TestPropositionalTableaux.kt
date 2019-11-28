661fdc988e20b3fcecf2393cedbeadc70985cddf
rec0de <mail@rec0de.net>
2019-11-28 14:31:18 +0100

    Add tests for us-4

--- backend/src/test/kotlin/kalkulierbar/TestPropositionalTableaux.kt
+++ backend/src/test/kotlin/kalkulierbar/TestPropositionalTableaux.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 class TestPropositionalTableaux {
 
-    var parser = PropositionalTableaux()
+    val instance = PropositionalTableaux()
 
     val invalidString1 = "a,b;c,!d;e,&;g,h,i,!j"
     val invalidString2 = "richtig; oder,!falsch"
@@ -22,28 +22,32 @@ class TestPropositionalTableaux {
     val edgeCase2 = "kein,valName,!"
     val edgeCase3 = "doppelter;Semikolon;;hello"
 
+    /*
+        Test parseFormulaToState
+    */
+
     @Test
-    fun testInvalidStrings() {
+    fun testParseInvalidStrings() {
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(invalidString1)
+            instance.parseFormulaToState(invalidString1)
         }
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(invalidString2)
+            instance.parseFormulaToState(invalidString2)
         }
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(invalidString3)
+            instance.parseFormulaToState(invalidString3)
         }
     }
 
     @Test
-    fun testValidString() {
-        val state1 = parser.parseFormulaToState(validString1)
-        val state2 = parser.parseFormulaToState(validString2)
-        val state3 = parser.parseFormulaToState(validString3)
+    fun testParseValidString() {
+        val state1 = instance.parseFormulaToState(validString1)
+        val state2 = instance.parseFormulaToState(validString2)
+        val state3 = instance.parseFormulaToState(validString3)
 
-        var root1 = state1.nodes[0]
-        var root2 = state2.nodes[0]
-        var root3 = state3.nodes[0]
+        val root1 = state1.nodes[0]
+        val root2 = state2.nodes[0]
+        val root3 = state3.nodes[0]
 
         assertEquals(state1.nodes.size, 1)
         assertEquals(root1.parent, 0)
@@ -62,18 +66,152 @@ class TestPropositionalTableaux {
     }
 
     @Test
-    fun testEdgeCases() {
+    fun testParseEdgeCases() {
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(emptyString)
+            instance.parseFormulaToState(emptyString)
         }
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(edgeCase1)
+            instance.parseFormulaToState(edgeCase1)
         }
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(edgeCase2)
+            instance.parseFormulaToState(edgeCase2)
         }
         Assertions.assertThrows(InvalidFormulaFormat::class.java) {
-            parser.parseFormulaToState(edgeCase3)
+            instance.parseFormulaToState(edgeCase3)
         }
+    }
+
+    /*
+        Test applyMoveOnState
+    */
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testUnknownMove() {
+        var state = instance.parseFormulaToState("a,b,c;d")
+        val hash = state.getHash()
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"d\", \"id1\": 1, \"id2\": 0}")
+        }
+
+        Assertions.assertEquals(hash, state.getHash()) // Verify that state has not been modified
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandValidA() {
+        var state = instance.parseFormulaToState("a,b,c;d")
+
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 0}")
+
+        Assertions.assertEquals(4, state.nodes.size)
+        Assertions.assertEquals(3, state.nodes.get(0).children.size)
+        Assertions.assertEquals("tableauxstate|{a, b, c}, {d}|[true;p;0;-;i;o;(1,2,3)|a;p;0;-;l;o;()|b;p;0;-;l;o;()|c;p;0;-;l;o;()]", state.getHash())
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandValidB() {
+        var state = instance.parseFormulaToState("a,b,c;d")
+
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 1}")
+
+        Assertions.assertEquals(2, state.nodes.size)
+        Assertions.assertEquals(1, state.nodes.get(0).children.size)
+        Assertions.assertEquals("tableauxstate|{a, b, c}, {d}|[true;p;0;-;i;o;(1)|d;p;0;-;l;o;()]", state.getHash())
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandValidC() {
+        var state = instance.parseFormulaToState("a,b,c;d")
+
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 0}")
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 3, \"id2\": 1}")
+
+        Assertions.assertEquals(5, state.nodes.size)
+        Assertions.assertEquals(3, state.nodes.get(0).children.size)
+        Assertions.assertEquals(1, state.nodes.get(3).children.size)
+        Assertions.assertEquals("tableauxstate|{a, b, c}, {d}|[true;p;0;-;i;o;(1,2,3)|a;p;0;-;l;o;()|b;p;0;-;l;o;()|c;p;0;-;i;o;(4)|d;p;3;-;l;o;()]", state.getHash())
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandLeafIndexOOB() {
+        var state = instance.parseFormulaToState("a,b;c")
+
+        val hash = state.getHash()
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 1, \"id2\": 0}")
+        }
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": -15, \"id2\": 0}")
+        }
+
+        Assertions.assertEquals(hash, state.getHash()) // Verify that state has not been modified
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandClauseIndexOOB() {
+        var state = instance.parseFormulaToState("a,b;c")
+
+        val hash = state.getHash()
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 2}")
+        }
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": -3}")
+        }
+
+        Assertions.assertEquals(hash, state.getHash()) // Verify that state has not been modified
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandOnNonLeaf() {
+        var state = instance.parseFormulaToState("a,b;c")
+
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 1}")
+        state = instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 1, \"id2\": 1}")
+
+        val hash = state.getHash()
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 0, \"id2\": 0}")
+        }
+
+        Assertions.assertThrows(IllegalMove::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": 1, \"id2\": 0}")
+        }
+
+        Assertions.assertEquals(hash, state.getHash()) // Verify that state has not been modified
+    }
+
+    @Test
+    @kotlinx.serialization.UnstableDefault
+    fun testExpandNullValues() {
+        var state = instance.parseFormulaToState("a,b;c")
+
+        val hash = state.getHash()
+
+        Assertions.assertThrows(JsonParseException::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":\"e\", \"id1\": null, \"id2\": 2}")
+        }
+
+        Assertions.assertThrows(JsonParseException::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":null, \"id1\": 0, \"id2\": -3}")
+        }
+
+        Assertions.assertThrows(JsonParseException::class.java) {
+            instance.applyMoveOnState(state, "{\"type\":null, \"id1\": 0, \"id2\": null}")
+        }
+
+        Assertions.assertEquals(hash, state.getHash()) // Verify that state has not been modified
     }
 }
