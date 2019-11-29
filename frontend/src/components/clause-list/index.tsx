bc3873edf5e75402e02a76d162faa59e141f0034
Julius Henke <juliusb.henke@gmail.com>
2019-11-29 11:17:34 +0100

    add style for selected clause in list

--- frontend/src/components/clause-list/index.tsx
+++ frontend/src/components/clause-list/index.tsx
@@ -1,4 +1,5 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import { clauseToString } from "../../helpers/clause";
 import { ClauseSet } from "../../types/clause";
 import * as style from "./style.css";
@@ -12,10 +13,33 @@ interface Props {
 
 // Displays clauses of the set as a list.
 const ClauseList: preact.FunctionalComponent<Props> = ({ clauseSet }) => {
+    const [userInput, setUserInput] = useState("");
+
+    // Handle Click event
+    const onClick = ({ target }: Event) => {
+        const { textContent, classList} = target as HTMLParagraphElement;
+        setUserInput("" + textContent);
+
+        // Remove selected style from all previous selected clauses
+        const selectedClauses = document.getElementsByClassName(style.clauseSelected);
+        for(let i = 0; i < selectedClauses.length; i++){
+            const item = selectedClauses.item(i)
+            if(item){
+                item.classList.remove(style.clauseSelected);
+            }
+        }
+
+        // Add selected style to new clause
+        classList.add(style.clauseSelected);
+
+        console.log(userInput);
+    };
+    
+
     return (
         <div class="card">
             {clauseSet.clauses.map(c => (
-                <p class={style.clauseListItem}>{clauseToString(c)}</p>
+                <p onClick={onClick} class={style.clauseListItem} >{clauseToString(c)}</p>
             ))}
         </div>
     );
