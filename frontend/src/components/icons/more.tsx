73134c75d6f903b3ef0a9c766b175bc91be4f2f8
drodt <d.drodt@gmx.de>
2019-12-05 16:24:33 +0100

    Add more icon

---
+++ frontend/src/components/icons/more.tsx
@@ -0,0 +1,23 @@
+import { h } from "preact";
+
+interface Props {
+    size?: number;
+    fill: string;
+}
+
+const MoreIcon: preact.FunctionalComponent<Props> = ({ size = 24, fill }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+    >
+        <path fill="none" d="M0 0h24v24H0V0z" />
+        <path
+            fill={fill}
+            d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+        />
+    </svg>
+);
+
+export default MoreIcon;
