a203319931957e2dd2e48fbe7b1900643e39a263
Kumiho <kumiho@rec0de.net>
2019-12-13 08:53:16 +0100

    Add tests for checkRegularity

---
+++ backend/src/test/kotlin/kalkulierbar/TestRegularity.kt
@@ -0,0 +1,149 @@
+package kalkulierbar.tests
+
+import kalkulierbar.tableaux.PropositionalTableaux
+import kalkulierbar.tableaux.TableauxNode
+import kalkulierbar.tableaux.TableauxParam
+import kalkulierbar.tableaux.TableauxState
+import kalkulierbar.tableaux.TableauxType
+import kalkulierbar.tableaux.checkRegularity
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestRegularity {
+    val instance = PropositionalTableaux()
+    val opts = TableauxParam(TableauxType.UNCONNECTED, true)
+
+    // State creation helper function
+    private fun createState(nodes: List<TableauxNode>, state: TableauxState): TableauxState {
+        state.nodes.addAll(nodes)
+
+        for (i in nodes.indices) {
+            val parentThisNode = nodes[i].parent
+            state.nodes[parentThisNode!!].children.add(i + 1)
+        }
+        return state
+    }
+
+    @Test
+    fun testRegularityValidA() {
+        var state = instance.parseFormulaToState("a,b;!a;!b", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "a", false),
+                TableauxNode(0, "b", false),
+                TableauxNode(2, "b", true),
+                TableauxNode(1, "a", true)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(true, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityValidB() {
+        var state = instance.parseFormulaToState("a,b;!a;!b;a", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "a", false),
+                TableauxNode(0, "b", false),
+                TableauxNode(1, "a", true),
+                TableauxNode(2, "b", true),
+                TableauxNode(4, "a", false)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(true, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityValidC() {
+        var state = instance.parseFormulaToState("true,false;!true", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "true", false),
+                TableauxNode(0, "false", false),
+                TableauxNode(1, "true", true)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(true, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityValidD() {
+        var state = instance.parseFormulaToState("true,false;!true", opts)
+        assertEquals(true, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityValidE() {
+        var state = instance.parseFormulaToState("a", opts)
+        state = createState(listOf(TableauxNode(0, "a", false)), state)
+        assertEquals(true, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityInvalidA() {
+        var state = instance.parseFormulaToState("a,b;a;b;!a;!b", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "a", false),
+                TableauxNode(1, "a", false)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(false, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityInvalidB() {
+        var state = instance.parseFormulaToState("a,b;a;b;!a;!b", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "a", false),
+                TableauxNode(1, "a", true),
+                TableauxNode(2, "b", false),
+                TableauxNode(3, "a", false)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(false, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityInvalidC() {
+        var state = instance.parseFormulaToState("a,b;a;b;!a;!b", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "a", false),
+                TableauxNode(0, "b", false),
+                TableauxNode(1, "b", false),
+                TableauxNode(2, "a", false),
+                TableauxNode(2, "b", false)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(false, checkRegularity(state))
+    }
+
+    @Test
+    fun testRegularityInvalidD() {
+        var state = instance.parseFormulaToState("true;!true", opts)
+
+        val nodes = listOf(
+                TableauxNode(0, "true", false),
+                TableauxNode(1, "true", true),
+                TableauxNode(2, "true", false)
+        )
+
+        state = createState(nodes, state)
+
+        assertEquals(false, checkRegularity(state))
+    }
+}
