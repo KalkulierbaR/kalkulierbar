02627d4ec167697946dfc9d81f389035ae8e313b
drodt <d.drodt@gmx.de>
2019-12-29 15:25:56 +0100

    Add more parser tests

--- backend/src/test/kotlin/kalkulierbar/TestPropParser.kt
+++ backend/src/test/kotlin/kalkulierbar/TestPropParser.kt
@@ -2,21 +2,50 @@ package kalkulierbar
 
 import kalkulierbar.parsers.PropositionalParser
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 
 class TestPropParser {
     private val parser = PropositionalParser()
 
-    private val invalid = listOf("", "-->a", "<--", "--><=>", "!->", "a!", "a-->", "b<=>")
+    private val invalid = listOf(
+            "",
+            "-->a",
+            "<--",
+            "--><=>",
+            "!->",
+            "a!",
+            "a-->",
+            "b<=>",
+            "<->a",
+            "<->")
+
+    private val valid = mapOf(
+            "a" to "a",
+            "!a" to "!a",
+            "a -> b" to "(a --> b)",
+            //"a --> b" to "(a --> b),"
+            "a-> b" to "(a --> b)",
+            "a    ->b" to "(a --> b)",
+            "a->b" to "(a --> b)",
+            "a<->(b -> (!(c)))" to "(a <=> (b --> !c))",
+            "(b & a <-> (a) | !b)" to "((b ∧ a) <=> (a ∨ !b))"
+    )
+
+    @Test
+    fun testInvalidStrings() {
+        for (s in invalid) {
+            assertFailsWith<InvalidFormulaFormat> {
+                parser.parse(s)
+            }
+        }
+    }
 
     @Test
     fun testValidStrings() {
-        for(s in invalid) {
-            assertFailsWith<InvalidFormulaFormat> {
-                println(s)
-                parser.parse(s)
-            }
+        for ((formula, expected) in valid) {
+            assertEquals(expected, parser.parse(formula).toString())
         }
     }
 }
