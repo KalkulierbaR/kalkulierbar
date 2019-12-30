82a000a8039127b46a168f0d5d8462e6948f6a01
drodt <d.drodt@gmx.de>
2019-12-30 15:36:22 +0100

    Add formula format explainer

--- frontend/src/routes/prop-tableaux/index.tsx
+++ frontend/src/routes/prop-tableaux/index.tsx
@@ -84,6 +84,24 @@ const Tableaux: preact.FunctionalComponent<Props> = ({
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <h3>Format</h3>
+                <p>Formulas can be entered in one two different formats:</p>
+                <h4>Clause Sets</h4>
+                <p>
+                    <code>a,!c;b</code>
+                </p>
+                <p>
+                    This formula represents the clause set{" "}
+                    <code>{"{{a, ¬c}, {b}}"}</code>
+                </p>
+                <p>Instead of semicolons, line breaks can be used.</p>
+                <h4>Propositional Formulas</h4>
+                <p>
+                    <code>{"a -> ( b & !c <=> a)"}</code>
+                </p>
+                <p>{"<=> and <->"} are synonymous.</p>
+            </div>
         </Fragment>
     );
 };
