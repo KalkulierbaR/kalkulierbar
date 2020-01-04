da398e4fea4d73ee73520f4324ee6f1724d4889a
drodt <d.drodt@gmx.de>
2020-01-04 23:14:05 +0100

    Update example state

--- frontend/src/routes/prop-resolution/view/example.ts
+++ frontend/src/routes/prop-resolution/view/example.ts
@@ -1,29 +1,46 @@
-import {ResolutionState} from "../../../types/resolution";
+import { ResolutionState } from "../../../types/resolution";
 
 const example: ResolutionState = {
     clauseSet: {
-        "clauses": [{
-            "atoms": [{"lit": "a", "negated": false}, {"lit": "b", "negated": true}, {
-                "lit": "c",
-                "negated": false
-            }]
-        }, {
-            "atoms": [{"lit": "c", "negated": true}, {"lit": "a", "negated": true}, {
-                "lit": "d",
-                "negated": false
-            }]
-        }, {"atoms": [{"lit": "d", "negated": false}, {"lit": "c", "negated": false}]}, {
-            "atoms": [{
-                "lit": "b",
-                "negated": true
-            }, {"lit": "a", "negated": false}]
-        }, {"atoms": [{"lit": "a", "negated": true}]}, {
-            "atoms": [{"lit": "b", "negated": false}, {
-                "lit": "c",
-                "negated": false
-            }]
-        }, {"atoms": [{"lit": "c", "negated": true}]}]
-    }, "seal": "E1018A62BF9682AEC7ACCCC21D00CF0D646D20DDBCEE446E3688A829FD0162CF"
+        clauses: [
+            {
+                atoms: [
+                    { lit: "a", negated: false },
+                    { lit: "b", negated: true },
+                    { lit: "c", negated: false }
+                ]
+            },
+            {
+                atoms: [
+                    { lit: "c", negated: true },
+                    { lit: "a", negated: true },
+                    { lit: "d", negated: false }
+                ]
+            },
+            {
+                atoms: [
+                    { lit: "d", negated: false },
+                    { lit: "c", negated: false }
+                ]
+            },
+            {
+                atoms: [
+                    { lit: "b", negated: true },
+                    { lit: "a", negated: false }
+                ]
+            },
+            { atoms: [{ lit: "a", negated: true }] },
+            {
+                atoms: [
+                    { lit: "b", negated: false },
+                    { lit: "c", negated: false }
+                ]
+            },
+            { atoms: [{ lit: "c", negated: true }] }
+        ]
+    },
+    highlightSelectable: true,
+    seal: "1B1EA116706E73E55AE0B2CADA263D48086737C38D33E05CD760ED5852A0085C"
 };
 
 export default example;
