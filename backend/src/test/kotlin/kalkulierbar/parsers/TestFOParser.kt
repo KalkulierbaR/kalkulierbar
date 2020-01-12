f8a006b51eb91ae9846a7947a347931481e90afc
drodt <d.drodt@gmx.de>
2020-01-12 14:13:04 +0100

    Add FO Parser test

---
+++ backend/src/test/kotlin/kalkulierbar/parsers/TestFOParser.kt
@@ -0,0 +1,53 @@
+package kalkulierbar.parsers
+
+import kalkulierbar.InvalidFormulaFormat
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TestFOParser {
+    private val parser = FirstOrderParser()
+
+    private val invalid = listOf(
+            "",
+            "-->a",
+            "<--",
+            "--><=>",
+            "!->",
+            "a!",
+            "a-->",
+            "b<=>",
+            "<->a",
+            "<->",
+            "(a&b v2",
+            "(a|b",
+            "\\all X: P(Y)",
+            "\\all x: P(X)",
+            "\\all X P(X)")
+
+    private val valid = mapOf(
+            "\\all X: P(X)" to "(∀X: P(X))",
+            "\\all X:P(X)" to "(∀X: P(X))",
+            "\\all X:           P(X)" to "(∀X: P(X))",
+            "\\all X: \\all Y: \\all Z: R(m(X, m(Y, Z)), m(m(X,Y, Z)))" to "(∀X: (∀Y: (∀Z: R(m(X, m(Y, Z)), m(m(X, Y, Z))))))",
+            "\\ex Xyz: P(Xyz)" to "(∃Xyz: P(Xyz))",
+            //"!\\ex X: (P(X) <-> !P(X))" to "!(∃X: (P(X) <=> !P(X)))",
+            "!(\\ex X: (P(X) <-> !P(X)))" to "!(∃X: (P(X) <=> !P(X)))"
+    )
+
+    @Test
+    fun testInvalidStrings() {
+        for (s in invalid) {
+            assertFailsWith<InvalidFormulaFormat> {
+                parser.parse(s)
+            }
+        }
+    }
+
+    @Test
+    fun testValidStrings() {
+        for ((formula, expected) in valid) {
+            assertEquals(expected, parser.parse(formula).toString())
+        }
+    }
+}
