4ecc17733fc85fc23aaa99520b9929be47db7fa6
drodt <d.drodt@gmx.de>
2020-01-12 14:38:08 +0100

    Add more tests

--- backend/src/test/kotlin/kalkulierbar/parsers/TestFOParser.kt
+++ backend/src/test/kotlin/kalkulierbar/parsers/TestFOParser.kt
@@ -21,24 +21,30 @@ class TestFOParser {
             "<->",
             "(a&b v2",
             "(a|b",
-            "\\all X: P(Y)",
+            "X",
+            "R(X)",
+            "\\all X: P(\\all: X: P(X))",
+            //"\\all X: P(Y)",
             "\\all x: P(X)",
             "\\all X P(X)")
 
     private val valid = mapOf(
+            "P(c)" to "P(c)",
             "\\all X: P(X)" to "(∀X: P(X))",
             "\\all X:P(X)" to "(∀X: P(X))",
             "\\all X:           P(X)" to "(∀X: P(X))",
             "\\all X: \\all Y: \\all Z: R(m(X, m(Y, Z)), m(m(X,Y, Z)))" to "(∀X: (∀Y: (∀Z: R(m(X, m(Y, Z)), m(m(X, Y, Z))))))",
             "\\ex Xyz: P(Xyz)" to "(∃Xyz: P(Xyz))",
             //"!\\ex X: (P(X) <-> !P(X))" to "!(∃X: (P(X) <=> !P(X)))",
-            "!(\\ex X: (P(X) <-> !P(X)))" to "!(∃X: (P(X) <=> !P(X)))"
+            "!(\\ex X: (P(X) <-> !P(X)))" to "!(∃X: (P(X) <=> !P(X)))",
+            "\\ex Xyz: P(Xyz) & \\all X: P(X)" to "((∃Xyz: P(Xyz)) ∧ (∀X: P(X)))"
     )
 
     @Test
     fun testInvalidStrings() {
         for (s in invalid) {
             assertFailsWith<InvalidFormulaFormat> {
+                //println(s)
                 parser.parse(s)
             }
         }
@@ -47,6 +53,7 @@ class TestFOParser {
     @Test
     fun testValidStrings() {
         for ((formula, expected) in valid) {
+            //println(formula)
             assertEquals(expected, parser.parse(formula).toString())
         }
     }
