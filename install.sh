e784c9d0ce6bf5b1ce283a8192a0b59d5defb3f5
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-19 16:08:51 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Install Node.JS (Ubuntu v13.3.0)
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+#Install yarn (Ubuntu v1.21.1)
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
