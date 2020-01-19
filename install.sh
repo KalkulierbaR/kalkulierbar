a2380de1192962b1bfc098e61fa6c50925681cb9
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-19 16:56:42 +0100

    Optional java installation

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
+echo "This will install java v11.0.5 (optional), curl, Node.JS and yarn!"
+echo " "
+echo "Installing Java v11.0.5"
+echo "Continue? [yes/no]"
+
+read opt
+	
+#Install java (v11.0.5)
+if ["$(opt)" == 'yes']; then
+	sudo apt-get install openjdk-11-jre-headles
+#If input does not match neither yes or no -> go to end
+elif ["$(opt)" != 'no']; then
+	exit 1
+fi 
+
 #Install curl
 sudo apt-get install curl
 
@@ -10,3 +25,4 @@ sudo apt-get install -y build-essential
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
+
