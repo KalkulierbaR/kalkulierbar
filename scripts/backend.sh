c5973a32d0d500dfc30939635e798b7964ec18bb
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-21 22:21:55 +0100

    Works properly

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -2,13 +2,11 @@
 pkill -f 'gradle'
 
 cd ../backend || exit
-if [ -f './gradlew' ]
-then
+if [ -f './gradlew' ]; then
   ./gradlew > /dev/null 2>&1
   ./gradlew run --args='--global' > /dev/null 2>&1
-elif [ -x "$(command -v gradle)" ]
+elif [ -x "$(command -v gradle)" ]; then
   gradle && gradle run --args='--global' > /dev/null 2>&1
-then
 else
   echo 'ERROR: No Gradle wrapper or programm found. Please install Gradle first.'
 fi
