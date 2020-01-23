4e9b9e7640f8eda0bf9d941f26b52c5ad05f8681
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-23 13:17:53 +0100

    Installs all dependencies

--- install.sh
+++ install.sh
@@ -11,20 +11,20 @@ else
     echo "Java found on your system"
 fi
 
-#Check if curl is installed
-if [ ! -z "$(command -v curl)" ]; then
+#Check if curl is installed (default for non empty string is -n)
+if [ "$(command -v curl)" ]; then
   echo "Curl found on your system"
-elif [ ! -z "$(command -v apt-get)" ]; then
+elif [ "$(command -v apt-get)" ]; then
   #Install curl debian/ubuntu
   sudo apt-get update
   sudo apt-get install curl
-elif [ ! -z "$(command -v yum)" ]; then
+elif [ "$(command -v yum)" ]; then
   #Install curl RHEL/CentOS/Fedora
   yum install curl
-elif [ ! -z "$(command -v zypper)" ]; then
+elif [ "$(command -v zypper)" ]; then
   #Install curl OpenSUSE
   zypper install curl
-elif [ ! -z "$(command -v pacman)" ]; then
+elif [ "$(command -v pacman)" ]; then
   #Install curl ArchLinux
   pacman -Sy curl
 else
@@ -39,9 +39,9 @@ sudo mkdir -p /usr/local/lib/nodejs
 sudo tar -xJvf node.tar.xz -C /usr/local/lib/nodejs
 rm node.tar.xz
 
-#curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-#sudo apt-get install -y build-essential
+#Export node path
+export PATH=/usr/local/lib/nodejs/node-v12.14.1-linux-x64/bin:$PATH
+. ~/.profile
 
 #Install yarn (Ubuntu v1.21.1)
 curl -o- -L https://yarnpkg.com/install.sh | bash
