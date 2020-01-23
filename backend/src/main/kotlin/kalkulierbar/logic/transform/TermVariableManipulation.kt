f841aa721eda98a075df19f91bbd62db9aff0838
Kumiho <kumiho@rec0de.net>
2020-01-23 21:43:54 +0100

    Fix unification and move to transform package

--- backend/src/main/kotlin/kalkulierbar/logic/transform/TermVariableManipulation.kt
+++ backend/src/main/kotlin/kalkulierbar/logic/transform/TermVariableManipulation.kt
@@ -4,8 +4,22 @@ import kalkulierbar.logic.Constant
 import kalkulierbar.logic.FirstOrderTerm
 import kalkulierbar.logic.Function
 import kalkulierbar.logic.QuantifiedVariable
+import kalkulierbar.logic.Relation
 
 class VariableInstantiator(val replacementMap: Map<String, FirstOrderTerm>) : FirstOrderTermVisitor<FirstOrderTerm>() {
+
+    companion object {
+        fun transform(term: FirstOrderTerm, map: Map<String, FirstOrderTerm>): FirstOrderTerm {
+            val instance = VariableInstantiator(map)
+            return term.accept(instance)
+        }
+
+        fun transformRelation(relation: Relation, map: Map<String, FirstOrderTerm>): Relation {
+            val instance = VariableInstantiator(map)
+            relation.arguments = relation.arguments.map { it.accept(instance) }
+            return relation
+        }
+    }
 
     override fun visit(node: QuantifiedVariable): FirstOrderTerm {
         if (replacementMap[node.spelling] != null) {
@@ -39,4 +53,20 @@ class VariableSuffixAppend(val suffix: String) : FirstOrderTermVisitor<FirstOrde
 
         return node
     }
+}
+
+class TermContainsVariable(val variable: String) : FirstOrderTermVisitor<Boolean>() {
+
+    companion object {
+        fun check(term: FirstOrderTerm, variable: String): Boolean {
+            val instance = TermContainsVariable(variable)
+            return term.accept(instance)
+        }
+    }
+
+    override fun visit(node: QuantifiedVariable) = (node.spelling == variable)
+
+    override fun visit(node: Constant) = false
+
+    override fun visit(node: Function) = node.arguments.fold(false) { acc, term -> acc || term.accept(this) }
 }
---
+++ backend/src/main/kotlin/kalkulierbar/logic/transform/Unification.kt
@@ -0,0 +1,77 @@
+package kalkulierbar.logic.transform
+
+import kalkulierbar.IllegalMove
+import kalkulierbar.logic.FirstOrderTerm
+import kalkulierbar.logic.Function
+import kalkulierbar.logic.QuantifiedVariable
+import kalkulierbar.logic.Relation
+
+/**
+ * Implements functions for a first order unification
+ */
+class Unification {
+    companion object {
+
+        /**
+         * Unifies two given relations by using "Robinson's Algorithm" (1963)
+         * @param r1 the first relation to be unified with r2
+         * @param r2 the second relation to be unified with r1
+         * @return a map of the executed substitutions
+         */
+        fun unify(r1: Relation, r2: Relation): Map<String, FirstOrderTerm> {
+            var rel1 = r1
+            var rel2 = r2
+            val arg1 = rel1.arguments
+            val arg2 = rel2.arguments
+
+            // Spelling has to be the same
+            if (rel1.spelling != rel2.spelling)
+                throw IllegalMove("Relations '$r1' and '$r2' have different names")
+            // Arg size has to be the same length
+            if (arg1.size != arg2.size)
+                throw IllegalMove("Relations '$r1' and '$r2' have different numbers of arguments")
+
+            val terms = mutableListOf<Pair<FirstOrderTerm, FirstOrderTerm>>()
+
+            for (i in arg1.indices)
+                terms.add(Pair(arg1[i], arg2[i]))
+
+            return unifyTerms(terms)
+        }
+
+        private fun unifyTerms(terms: MutableList<Pair<FirstOrderTerm, FirstOrderTerm>>): Map<String, FirstOrderTerm> {
+            val map = mutableMapOf<String, FirstOrderTerm>()
+
+            // As long as both relations aren't the same
+            while (terms.isNotEmpty()) {
+
+                var (term1, term2) = terms.removeAt(0)
+
+                // Apply gathered substitutions just-in-time
+                term1 = VariableInstantiator.transform(term1, map)
+                term2 = VariableInstantiator.transform(term2, map)
+
+                // Skip terms if already equal
+                if (term1 == term2) {
+                    continue
+                } else if (term1 is QuantifiedVariable) {
+                    // Unification not possible if one is variable and appears in others arguments
+                    if (term2 is Function && TermContainsVariable.check(term2, term1.spelling))
+                        throw IllegalMove("Variable '$term1' appears in '$term2'")
+
+                    // Add substitution to map
+                    map.put(term1.spelling, term2)
+                } else if (term2 is QuantifiedVariable) {
+                    // Swap them around to be processed later
+                    terms.add(Pair(term2, term1))
+                } else if (term1 is Function && term2 is Function && term1.spelling == term2.spelling && term1.arguments.size == term2.arguments.size) {
+                    // Break down into subterms
+                    for (i in term1.arguments.indices)
+                        terms.add(Pair(term1.arguments[i], term2.arguments[i]))
+                } else
+                    throw IllegalMove("Cannot unify '$term1' and '$term2'")
+            }
+            return map
+        }
+    }
+}
