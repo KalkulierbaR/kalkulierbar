eeb82c570287a3382b92e5d1f42a43d1711eaa8f
Kumiho <kumiho@rec0de.net>
2020-01-24 10:39:27 +0100

    Resolve * imports

--- backend/src/test/kotlin/kalkulierbar/logic/TestFOLogic.kt
+++ backend/src/test/kotlin/kalkulierbar/logic/TestFOLogic.kt
@@ -1,11 +1,24 @@
 package kalkulierbar.tests.logic
 
 import kalkulierbar.FormulaConversionException
-import kalkulierbar.logic.*
+import kalkulierbar.logic.And
+import kalkulierbar.logic.Constant
+import kalkulierbar.logic.ExistentialQuantifier
 import kalkulierbar.logic.Function
+import kalkulierbar.logic.Impl
+import kalkulierbar.logic.Not
+import kalkulierbar.logic.Or
+import kalkulierbar.logic.QuantifiedVariable
+import kalkulierbar.logic.Relation
+import kalkulierbar.logic.UniversalQuantifier
+import kalkulierbar.logic.Var
 import kalkulierbar.logic.transform.NaiveCNF
 import kalkulierbar.logic.transform.TseytinCNF
-import kotlin.test.*
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotEquals
 
 class TestFOLogic {
     private lateinit var r1: Relation
