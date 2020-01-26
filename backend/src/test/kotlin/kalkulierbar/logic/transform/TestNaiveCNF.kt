bca379924bedb944cbd077ef690eceee203cad26
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-26 15:14:09 +0100

    TestNaiveCNF

---
+++ backend/src/test/kotlin/kalkulierbar/logic/transform/TestNaiveCNF.kt
@@ -0,0 +1,29 @@
+package kalkulierbar.logic.transform
+
+import kalkulierbar.parsers.PropositionalParser
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestNaiveCNF {
+
+    private val parser = PropositionalParser()
+
+    private val formulas = mapOf(
+            "a -> b" to "{!a, b}",
+            "!(a | b)" to "{!a}, {!b}",
+            "!(a & b)" to "{!a, !b}",
+            "!(!a)" to "{a}",
+            "!(a | b) -> !(!a & b)" to "{a, b, a, !b}",
+            "a | !b -> !a <-> b & !a | b" to "{!a, !a, a, !b}, {!a, !a, a}, {!a, !a, !b, a}, {!a, !a, !b}, " +
+                    "{b, !a, a, !b}, {b, !a, a}, {b, !a, !b, a}, {b, !a, !b}, {b, b, a, !b}, {b, b, a}, " +
+                    "{b, b, !b, a}, {b, b, !b}, {!a, b, a, !b}, {!a, b, a}, {!a, b, !b, a}, {!a, b, !b}"
+    )
+
+    @Test
+    fun testValid() {
+        for ((f, e) in formulas) {
+            val parsed = parser.parse(f)
+            assertEquals(e, NaiveCNF.transform(parsed).toString())
+        }
+    }
+}
