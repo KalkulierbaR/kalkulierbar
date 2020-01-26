941c9fbd1122f81e2384ca268dfb2849e08d0e95
Kumiho <kumiho@rec0de.net>
2020-01-26 17:50:04 +0100

    Fix and extend tests

--- backend/src/test/kotlin/kalkulierbar/logic/transform/TestSkolemization.kt
+++ backend/src/test/kotlin/kalkulierbar/logic/transform/TestSkolemization.kt
@@ -13,6 +13,7 @@ class TestSkolemization {
             "!(R(a) & R(b))" to "!(R(a) ∧ R(b))",
             "!(!R(a) <-> !R(a))" to "!(!R(a) <=> !R(a))",
 
+            "\\ex A: R(A) & Q(sk1)" to "(R(sk1) ∧ Q(usk1))",
             "!\\ex A : !(S(A) & !\\all B : (R(B) -> !R(A)))" to "!!(S(sk1) ∧ !(∀B: (R(B) --> !R(sk1))))",
             "!\\all A : (P(A) <-> \\ex C : (R(A) <-> !R(C)))" to "!(∀A: (P(A) <=> (R(A) <=> !R(sk1(A)))))",
             "!\\ex A : R(A) -> !\\all B : !(R(B) | !R(B))" to "(!R(sk1) --> !(∀B: !(R(B) ∨ !R(B))))"
