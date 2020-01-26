9ad80b98f1b6531eee5e948c484d8760f5a2fd53
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-01-26 16:31:45 +0100

    Lint error correction

--- backend/src/test/kotlin/kalkulierbar/logic/transform/TestSkolemNormalForm.kt
+++ backend/src/test/kotlin/kalkulierbar/logic/transform/TestSkolemNormalForm.kt
@@ -14,7 +14,7 @@ class TestSkolemNormalForm {
             "!(!R(a) <-> !R(a))" to "((R(a) ∨ R(a)) ∧ (!R(a) ∨ !R(a)))",
 
             "!\\ex A : !(S(A) & !\\all B : (R(B) -> !R(A)))" to "(∀A: (S(A) ∧ (R(sk1(A)) ∧ R(A))))",
-            //"!\\all A : (P(A) <-> \\ex C : (R(A) <-> !R(C)))" to "",
+            // "!\\all A : (P(A) <-> \\ex C : (R(A) <-> !R(C)))" to "",
             "!\\ex A : R(A) -> !\\all B : !(R(B) | !R(B))" to "(R(sk1) ∨ (R(sk2) ∨ !R(sk2)))"
     )
 
