fa5335d067cb75b4bb24fcc8d51c04d07a7da232
Julius Henke <juliusb.henke@gmail.com>
2020-01-27 19:00:18 +0100

    Add FO format skeleton

--- frontend/src/components/input/formula/format.tsx
+++ frontend/src/components/input/formula/format.tsx
@@ -1,32 +1,56 @@
-import { h } from "preact";
+import { Fragment, h } from "preact";
 
-const Format: preact.FunctionalComponent = () => (
+interface Props {
+    /**
+     * Display the format for First Order logic
+     */
+    foLogic?: boolean[];
+}
+
+const Format: preact.FunctionalComponent<Props> = ({
+   foLogic= false
+}) => (
     <div class="card">
         <h3>Format</h3>
         <ul>
-            <li>
-                <p>
-                    <b>Clause Sets</b>
-                </p>
-                <p>
-                    <code>{"{{a, ¬c}, {b}}"}</code>&nbsp;&nbsp;&nbsp;needs to be entered as&nbsp;&nbsp;&nbsp;<code>a,!c;b</code>
-                </p>
-                <p>
-                    Use a semicolon or linebreak to signal a new clause.
-                </p>
-            </li>
-            <br/>
-            <li>
-                <p>
-                    <b>Propositional Formulas</b>
-                </p>
-                <p>
-                    <code>{"!a -> ( a & b <=> a | b)"}</code>&nbsp;&nbsp;&nbsp;is a valid input.
-                </p>
-                <p>
-                    <code>{"<=>"}</code>&nbsp;&nbsp;&nbsp;and&nbsp;&nbsp;&nbsp;<code>{"<->"}</code>&nbsp;&nbsp;&nbsp;are synonymous.
-                </p>
-            </li>
+            {foLogic ?
+                <li>
+                    <p>
+                        <b>First Order Formulas</b>
+                    </p>
+                    <p>
+                        bla bli blup
+                    </p>
+                    <p>
+                        bli bla blup
+                    </p>
+                </li>
+                : <Fragment>
+                    <li>
+                        <p>
+                            <b>Clause Sets</b>
+                        </p>
+                        <p>
+                            <code>{"{{a, ¬c}, {b}}"}</code>&nbsp;&nbsp;&nbsp;needs to be entered as&nbsp;&nbsp;&nbsp;<code>a,!c;b</code>
+                        </p>
+                        <p>
+                            Use a semicolon or linebreak to signal a new clause.
+                        </p>
+                    </li>
+                    <br/>
+                    <li>
+                        <p>
+                            <b>Propositional Formulas</b>
+                        </p>
+                        <p>
+                            <code>{"!a -> ( a & b <=> a | b)"}</code>&nbsp;&nbsp;&nbsp;is a valid input.
+                        </p>
+                        <p>
+                            <code>{"<=>"}</code>&nbsp;&nbsp;&nbsp;and&nbsp;&nbsp;&nbsp;<code>{"<->"}</code>&nbsp;&nbsp;&nbsp;are synonymous.
+                        </p>
+                    </li>
+                </Fragment>
+                }
         </ul>
     </div>
 );
