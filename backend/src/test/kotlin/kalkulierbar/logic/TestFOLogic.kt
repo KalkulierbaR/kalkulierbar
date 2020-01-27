02db0962997a1a65a994b4bd9de6c3bf3573fd9c
Kumiho <kumiho@rec0de.net>
2020-01-27 19:35:46 +0100

    Add tests for equality

--- backend/src/test/kotlin/kalkulierbar/logic/TestFOLogic.kt
+++ backend/src/test/kotlin/kalkulierbar/logic/TestFOLogic.kt
@@ -19,7 +19,6 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.assertNotEquals
 
 class TestFOLogic {
     private lateinit var r1: Relation
