8dfab40b3ac05e8e26f7cef3e7f0db56b21d3045
Daniel <d.drodt@gmx.de>
2020-01-31 11:50:38 +0100

    Redo home page

--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -2,33 +2,55 @@ import { h } from "preact";
 import { Calculus } from "../../types/app";
 import * as style from "./style.scss";
 
+interface Route {
+    name: string;
+    href: Calculus;
+    imageURL: string;
+}
+
+const ROUTES: Route[] = [
+    {
+        href: Calculus.propTableaux,
+        name: "Propositional Tableaux",
+        imageURL: "/assets/calculi/prop-tab-dark.png"
+    },
+    {
+        href: Calculus.foTableaux,
+        name: "First Order Tableaux",
+        imageURL: "/assets/calculi/fo-tab-dark.png"
+    },
+    {
+        href: Calculus.propResolution,
+        name: "Propositional Resolution",
+        imageURL: "/assets/calculi/prop-res-dark.png"
+    }
+];
+
+interface CalculusItemProps {
+    route: Route;
+}
+
+const CalculusItem: preact.FunctionalComponent<CalculusItemProps> = ({
+    route: { href, name, imageURL }
+}) => {
+    return (
+        <div class={style.calculusItem}>
+            <img class={style.calculusItemImage} src={imageURL} alt={name} />
+            <a href={`/${href}`}>{name}</a>
+        </div>
+    );
+};
+
 const Home: preact.FunctionalComponent = () => {
     return (
         <div class={style.home}>
             <div className="card">
                 <h3>Choose a calculus</h3>
-                <ul class={style.calculusList}>
-                    <li class={style.calculusListItem}>
-                        <a href={"/" + Calculus.propTableaux}>
-                            Propositional Tableaux
-                        </a>
-                    </li>
-                    <li class={style.calculusListItem}>
-                        <a href={"/" + Calculus.foTableaux}>
-                            First Order Tableaux
-                        </a>
-                    </li>
-                    <li class={style.calculusListItem}>
-                        <a href={"/" + Calculus.propResolution}>
-                            Propositional Resolution
-                        </a>
-                    </li>
-                    <li class={style.calculusListItem}>
-                        <a href={"/" + Calculus.foResolution}>
-                            First Order Resolution
-                        </a>
-                    </li>
-                </ul>
+                <div class={style.calculusGrid}>
+                    {ROUTES.map(r => (
+                        <CalculusItem route={r} />
+                    ))}
+                </div>
             </div>
         </div>
     );
