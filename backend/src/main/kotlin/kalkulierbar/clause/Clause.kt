de9e313120ce5fea73865b42f3a8006758e9b574
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-02-12 19:22:51 +0100

    Add clause functionallity

--- backend/src/main/kotlin/kalkulierbar/clause/Clause.kt
+++ backend/src/main/kotlin/kalkulierbar/clause/Clause.kt
@@ -12,6 +12,32 @@ class Clause<AtomType>(var atoms: MutableList<Atom<AtomType>> = mutableListOf())
         c.forEach { add(it) }
     }
 
+    fun clone(): Clause<AtomType> {
+        val newClause = Clause<AtomType>()
+        for (c in atoms) {
+            newClause.add(c.copy())
+        }
+        return newClause
+    }
+
+    /**
+     * Factorizes this clause
+     * - Removes doubled literals
+     * - A FO-Clause should be unified before
+     */
+    fun factorize() {
+        // Remove equal literal
+        for (i in atoms.indices) {
+            for (j in atoms.indices) {
+                // Delete if not same position but same literal
+                if (i != j && atoms[i] == atoms[j]) {
+                    atoms.removeAt(j)
+                    continue
+                }
+            }
+        }
+    }
+
     override fun toString(): String {
         return "{${atoms.joinToString(", ")}}"
     }
