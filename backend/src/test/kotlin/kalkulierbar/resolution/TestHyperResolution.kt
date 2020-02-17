d1b9301ddae14208e1cb201ad19881ad271fed76
rec0de <mail@rec0de.net>
2020-02-17 21:39:25 +0100

    Add hyperres tests

---
+++ backend/src/test/kotlin/kalkulierbar/resolution/TestHyperResolution.kt
@@ -0,0 +1,100 @@
+package kalkulierbar.tests.resolution
+
+import kalkulierbar.IllegalMove
+import kalkulierbar.resolution.FirstOrderResolution
+import kalkulierbar.resolution.MoveHyper
+import kalkulierbar.resolution.PropositionalResolution
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TestHyperResolution {
+
+    val prop = PropositionalResolution()
+    val fo = FirstOrderResolution()
+
+    @Test
+    fun testIndexBoundsChecking() {
+        val state = prop.parseFormulaToState("a,!b,!c,!d; b,e; c,f; d,g,h", null)
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(-1, listOf()))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(4, listOf()))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(-1, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(4, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(1, -1))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(1, 2))))
+        }
+    }
+
+    @Test
+    fun testEmptySidePremiss() {
+        val state = prop.parseFormulaToState("a,!b,!c,!d; b,e; c,f; d,g,h", null)
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf()))
+        }
+    }
+
+    @Test
+    fun testNegativityConstraintA() {
+        val state = prop.parseFormulaToState("a,!b,!c,!d; b,e,!f; c; d", null)
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(1, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(2, 0), Pair(1, 0), Pair(3, 0))))
+        }
+    }
+
+    @Test
+    fun testNegativityConstraintB() {
+        val state = prop.parseFormulaToState("a,!b,c,!d; b,e,f; !c; d", null)
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(2, 0), Pair(1, 0), Pair(3, 0))))
+        }
+    }
+
+    @Test
+    fun testNegativityConstraintC() {
+        val state = prop.parseFormulaToState("a,!b,!c,!d; b,e; c;", null)
+
+        assertFailsWith<IllegalMove> {
+            prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(2, 0), Pair(1, 0))))
+        }
+    }
+
+    @Test
+    fun testValidA() {
+        var state = prop.parseFormulaToState("a,!b,!c,!d; b,e; c,f; g,d,h", null)
+        state = prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(2, 0), Pair(1, 0), Pair(3, 1))))
+
+        assertEquals("{a, !b, !c, !d}, {b, e}, {c, f}, {g, d, h}, {a, f, e, g, h}", state.clauseSet.toString())
+    }
+
+    @Test
+    fun testValidB() {
+        var state = prop.parseFormulaToState("a,!b,c,d; e,f,b,g", null)
+        state = prop.applyMoveOnState(state, MoveHyper(0, listOf(Pair(1, 2))))
+
+        assertEquals("{a, !b, c, d}, {e, f, b, g}, {a, c, d, e, f, g}", state.clauseSet.toString())
+    }
+}
