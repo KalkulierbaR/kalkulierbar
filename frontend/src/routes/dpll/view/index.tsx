0c11049298cad4d930e8d0538d6d6a294159f256
drodt <d.drodt@gmx.de>
2020-02-26 21:28:36 +0100

    Add example state

--- frontend/src/routes/dpll/view/index.tsx
+++ frontend/src/routes/dpll/view/index.tsx
@@ -2,21 +2,20 @@ import { Fragment, h } from "preact";
 import DPLLTree from "../../../components/dpll/tree";
 import { useAppState } from "../../../helpers/app-state";
 
+import dpllExampleState from "./example";
 import * as style from "./style.scss";
 
 interface Props {}
 
 const DPLLView: preact.FunctionalComponent<Props> = () => {
-    const { dpll: state } = useAppState();
+    const { dpll: cState } = useAppState();
 
-    if (!state) {
-        return <p>No State!</p>;
-    }
+    const state = cState || dpllExampleState;
 
     return (
         <Fragment>
             <h2>DPLL View</h2>
-            <div class={`card ${style.dpllView}`}>
+            <div class={style.dpllView}>
                 <DPLLTree nodes={state.tree} />
             </div>
         </Fragment>
--- frontend/src/types/dpll.ts
+++ frontend/src/types/dpll.ts
@@ -1,18 +1,43 @@
-import { ClauseSet } from "./clause";
+import { Clause, ClauseSet } from "./clause";
 
 export interface DPLLTreeNode {
     parent: number | null;
     type: DPLLNodeType;
     label: string;
     children: number[];
-    isLeaf: boolean;
-    isAnnotation: boolean;
-    modelVerified?: boolean;
+    modelVerified: boolean | null;
+    diff: DPLLCsDiff;
 }
+
+export interface DPLLCsDiffIdentity {
+    type: "cd-identity";
+}
+
+export interface DPLLCsDiffRemoveClause {
+    type: "cd-delclause";
+    id: number;
+}
+
+export interface DPLLCsDiffAddClause {
+    type: "cd-addclause";
+    clause: Clause;
+}
+
+export interface DPLLCsDiffRemoveAtom {
+    type: "cd-delatom";
+    cid: number;
+    aid: number;
+}
+
+export type DPLLCsDiff =
+    | DPLLCsDiffIdentity
+    | DPLLCsDiffRemoveClause
+    | DPLLCsDiffAddClause
+    | DPLLCsDiffRemoveAtom;
 
 export type DPLLTreeLayoutNode = DPLLTreeNode & { id: number };
 
-export enum DPLLNodeType {
+export const enum DPLLNodeType {
     ROOT = "ROOT",
     PROP = "PROP",
     SPLIT = "SPLIT",
@@ -23,6 +48,7 @@ export enum DPLLNodeType {
 export interface DPLLState {
     clauseSet: ClauseSet;
     tree: DPLLTreeNode[];
+    seal: string;
 }
 
 export interface DPLLSplit {
