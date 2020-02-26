192da5d36e9096dc7db9142e50db6c8b7bb1d452
Kumiho <kumiho@rec0de.net>
2020-02-26 13:52:29 +0100

    Add hyperres FO Tests

--- backend/src/test/kotlin/kalkulierbar/resolution/TestHyperResolution.kt
+++ backend/src/test/kotlin/kalkulierbar/resolution/TestHyperResolution.kt
@@ -101,4 +101,84 @@ class TestHyperResolution {
 
         assertEquals("{a, !b, c, d}, {e, f, b, g}, {a, c, d, e, f, g}", state.clauseSet.toString())
     }
+
+    @Test
+    fun testFoEmpty() {
+        val state = fo.parseFormulaToState("R(c)", null)
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf()))
+        }
+    }
+
+    @Test
+    fun testFoIndexChecks() {
+        val state = fo.parseFormulaToState("(S(a) | !S(b) | !S(c) | !S(d)) & (R(b) | R(c)) & (Q(c) | Q(k)) & (P(d) | P(g) | P(h))", null)
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(-1, mapOf()))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(4, mapOf()))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(0 to Pair(-1, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(0 to Pair(4, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(0 to Pair(1, -1))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(0 to Pair(1, 2))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(-1 to Pair(1, 0))))
+        }
+    }
+
+    @Test
+    fun testFoNegativity() {
+        val state = fo.parseFormulaToState("(R(c1) | !R(c2) | !R(c3)) & R(c3) & (R(c2) | !Q(c))", null)
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(2 to Pair(1, 0))))
+        }
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(2 to Pair(1, 0), 1 to Pair(2, 0))))
+        }
+    }
+
+    @Test
+    fun testFoUnification() {
+        val state = fo.parseFormulaToState("(R(c1) | !R(c2) | !R(c3)) & R(q) & (R(c2) | Q(c))", null)
+
+        assertFailsWith<IllegalMove> {
+            fo.applyMoveOnState(state, MoveHyper(0, mapOf(1 to Pair(1, 0), 2 to Pair(2, 0))))
+        }
+    }
+
+    @Test
+    fun testFoValidA() {
+        var state = fo.parseFormulaToState("/all Y: /all Z: (R(a) | !P(Y) | !Q(Z)) & (Q(f(c)) | R(c)) & P(g)", null)
+        state = fo.applyMoveOnState(state, MoveHyper(0, mapOf(1 to Pair(2, 0), 2 to Pair(1, 0))))
+
+        assertEquals("{R(a), !P(Y), !Q(Z)}, {Q(f(c)), R(c)}, {P(g)}, {R(a), R(c)}", state.clauseSet.toString())
+    }
+
+    @Test
+    fun testFoValidB() {
+        var state = fo.parseFormulaToState("/all X: (R(a) | !R(X) | R(X) | R(d)) & (R(e) | R(f) | R(b) | R(g))", null)
+        state = fo.applyMoveOnState(state, MoveHyper(0, mapOf(1 to Pair(1, 2))))
+
+        assertEquals("{R(a), !R(X), R(X), R(d)}, {R(e), R(f), R(b), R(g)}, {R(a), R(b), R(d), R(e), R(f), R(g)}", state.clauseSet.toString())
+    }
 }
