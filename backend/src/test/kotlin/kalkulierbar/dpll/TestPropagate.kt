032bec38ccc1f6cdc505af77e4582362ba52fd45
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-03-05 19:46:30 +0100

    Test Propagate adjust (Closed not working properly)

--- backend/src/test/kotlin/kalkulierbar/dpll/TestPropagate.kt
+++ backend/src/test/kotlin/kalkulierbar/dpll/TestPropagate.kt
@@ -53,6 +53,11 @@ class TestPropagate {
         assertEquals(false, state.tree[2].isLeaf)
         assertEquals(true, state.tree[3].isLeaf)
 
+        assertEquals(false, state.tree[0].isAnnotation)
+        assertEquals(false, state.tree[1].isAnnotation)
+        assertEquals(false, state.tree[2].isAnnotation)
+        assertEquals(true, state.tree[3].isAnnotation)
+
         assertEquals("true", state.tree[0].label)
         assertEquals("prop", state.tree[1].label)
         assertEquals("prop", state.tree[2].label)
@@ -60,14 +65,23 @@ class TestPropagate {
 
         assertEquals("[{a}, {!a, b}, {a, c}]", state.tree[0].diff.apply(state.clauseSet).clauses.toString())
         assertEquals("[{a}, {b}, {a, c}]", state.tree[1].diff.apply(state.clauseSet).clauses.toString())
-        var newClauseSet = state.tree[1].diff.apply(state.clauseSet)
+        val newClauseSet = state.tree[1].diff.apply(state.clauseSet)
         assertEquals("[{a}, {b}]", state.tree[2].diff.apply(newClauseSet).clauses.toString())
+    }
+
+    @Test
+    fun testValidClosed() {
+        var state = dpll.parseFormulaToState("a;a;a,b", null)
+        state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+
+        assertEquals(NodeType.CLOSED, state.tree[1].type)
+        assertEquals(NodeType.MODEL, state.tree[2].type)
     }
 
     @Test
     fun testInvalid() {
         var state = dpll.parseFormulaToState("a;!a,b;a,c", null)
-        // wrong param
+        // Out of bounds
         assertFailsWith<IllegalMove> {
             state = dpll.applyMoveOnState(state, MovePropagate(-1, 0, 1, 0))
         }
@@ -97,6 +111,11 @@ class TestPropagate {
             state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
             // Same branch twice
             state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 2, 0))
+        }
+        assertFailsWith<IllegalMove> { // Propagate Annotation
+            var state1 = dpll.parseFormulaToState("a;a,b", null)
+            state1 = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+            state1 = dpll.applyMoveOnState(state, MovePropagate(2, 0, 1, 0))
         }
         assertFailsWith<IllegalMove> {
             // Base clause with 2 objects
