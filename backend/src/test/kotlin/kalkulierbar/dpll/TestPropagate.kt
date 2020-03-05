1681c1a4d448cdf635300915c99dd52855a31015
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-03-05 11:52:09 +0100

    Test Move Propagate

---
+++ backend/src/test/kotlin/kalkulierbar/dpll/TestPropagate.kt
@@ -0,0 +1,110 @@
+package kalkulierbar.tests.dpll
+
+import kalkulierbar.IllegalMove
+import kalkulierbar.dpll.MovePropagate
+import kalkulierbar.dpll.NodeType
+import kalkulierbar.dpll.PropositionalDPLL
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TestPropagate {
+
+    val dpll = PropositionalDPLL()
+
+    @Test
+    fun testValidDiffers() {
+        var state = dpll.parseFormulaToState("a;!a,b,c", null)
+        state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+
+        assertEquals("{b, c}", state.tree[1].diff.apply(state.clauseSet).clauses[1].toString())
+        assertEquals(NodeType.PROP, state.tree[1].type)
+    }
+
+    @Test
+    fun testValidEquals() {
+        var state = dpll.parseFormulaToState("a;a,b,c", null)
+        state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+
+        assertEquals("{a}", state.tree[1].diff.apply(state.clauseSet).clauses[0].toString())
+        assertEquals(3, state.tree.size)
+        assertEquals(NodeType.MODEL, state.tree[2].type)
+    }
+
+    @Test
+    fun testValidMany() {
+        var state = dpll.parseFormulaToState("a;!a,b;a,c", null)
+        state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+        state = dpll.applyMoveOnState(state, MovePropagate(1, 0, 2, 0))
+
+        assertEquals(4, state.tree.size)
+        assertEquals(null, state.tree[0].parent)
+        assertEquals(0, state.tree[1].parent)
+        assertEquals(1, state.tree[2].parent)
+        assertEquals(2, state.tree[3].parent)
+
+        assertEquals(mutableListOf(1), state.tree[0].children)
+        assertEquals(mutableListOf(2), state.tree[1].children)
+        assertEquals(mutableListOf(3), state.tree[2].children)
+        assertEquals(mutableListOf(), state.tree[3].children)
+
+        assertEquals(false, state.tree[0].isLeaf)
+        assertEquals(false, state.tree[1].isLeaf)
+        assertEquals(false, state.tree[2].isLeaf)
+        assertEquals(true, state.tree[3].isLeaf)
+
+        assertEquals("true", state.tree[0].label)
+        assertEquals("prop", state.tree[1].label)
+        assertEquals("prop", state.tree[2].label)
+        assertEquals("model", state.tree[3].label)
+
+        assertEquals("[{a}, {!a, b}, {a, c}]", state.tree[0].diff.apply(state.clauseSet).clauses.toString())
+        assertEquals("[{a}, {b}, {a, c}]", state.tree[1].diff.apply(state.clauseSet).clauses.toString())
+        var newClauseSet = state.tree[1].diff.apply(state.clauseSet)
+        assertEquals("[{a}, {b}]", state.tree[2].diff.apply(newClauseSet).clauses.toString())
+    }
+
+    @Test
+    fun testInvalid() {
+        var state = dpll.parseFormulaToState("a;!a,b;a,c", null)
+        // wrong param
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(-1, 0, 1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, -1, 1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, -1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, -1))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(101, 0, 1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 101, 1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 101, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 101))
+        }
+        // Conflicts
+        assertFailsWith<IllegalMove> {
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 0))
+            // Same branch twice
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 2, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            // Base clause with 2 objects
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 1, 1, 0))
+        }
+        assertFailsWith<IllegalMove> {
+            // Wrong prop atom
+            state = dpll.applyMoveOnState(state, MovePropagate(0, 0, 1, 1))
+        }
+    }
+}
