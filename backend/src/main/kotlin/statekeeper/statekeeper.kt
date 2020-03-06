494dab498d64be1a21e746ccd8797aef5fb40e31
Kumiho <kumiho@rec0de.net>
2020-03-06 20:04:32 +0100

    Implement persistence

--- backend/src/main/kotlin/statekeeper/statekeeper.kt
+++ backend/src/main/kotlin/statekeeper/statekeeper.kt
@@ -1,5 +1,6 @@
 package statekeeper
 
+import java.io.File
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
@@ -13,34 +14,30 @@ import org.komputing.khash.keccak.extensions.digestKeccak
 class StateKeeper {
     companion object {
 
-        // TODO: Store this somewhere else
-        private val key = "correctHorseBatteryStaple"
-
         private val date
             get() = DateTimeFormatter.ofPattern("yyyyMMdd").withZone(ZoneOffset.UTC).format(Instant.now())
 
+        private val storage = File("kbar-state.json")
+        private val state: AppState
+
+        init {
+            try {
+                if (storage.createNewFile())
+                    state = AppState()
+                else
+                    state = Json.parse(AppState.serializer(), storage.readText())
+            } catch (e: Exception) {
+                val msg = "Could not parse stored state: "
+                throw JsonParseException(msg + (e.message ?: "Unknown error"))
+            }
+        }
+
         // TODO: Make these things persistent somehow
-        private val enabledCalculi = mutableListOf(
-            "prop-tableaux",
-            "prop-resolution",
-            "fo-tableaux",
-            "fo-resolution",
-            "prop-dpll"
-        )
-        private val examples = mutableListOf<Example>(
-            Example(
-                "Example Example",
-                "An example showing the capabilities of examples",
-                "prop-tableaux",
-                "a,b,c;!a;!b;!c",
-                "{\"type\": \"UNCONNECTED\", \"regular\": false, \"backtracking\": true}"
-            )
-        )
 
         @kotlinx.serialization.UnstableDefault
         fun getConfig(): String {
-            val calculiJson = enabledCalculi.map { "\"$it\"" }.joinToString(", ")
-            val examplesJson = examples.map { Json.stringify(Example.serializer(), it) }.joinToString(", ")
+            val calculiJson = state.enabledCalculi.map { "\"$it\"" }.joinToString(", ")
+            val examplesJson = state.examples.map { Json.stringify(Example.serializer(), it) }.joinToString(", ")
 
             return """{"calculi": [$calculiJson], "examples": [$examplesJson]}"""
         }
@@ -62,7 +59,8 @@ class StateKeeper {
 
             try {
                 val parsedExample = Json.parse(Example.serializer(), example)
-                examples.add(parsedExample)
+                state.examples.add(parsedExample)
+                flush()
             } catch (e: Exception) {
                 val msg = "Could not parse JSON example: "
                 throw JsonParseException(msg + (e.message ?: "Unknown error"))
@@ -79,9 +77,10 @@ class StateKeeper {
 
             try {
                 val id = exampleIdString.toInt()
-                if (id < 0 || id >= examples.size)
+                if (id < 0 || id >= state.examples.size)
                     throw JsonParseException("Example with ID $id does not exist")
-                examples.removeAt(id)
+                state.examples.removeAt(id)
+                flush()
             } catch (e: NumberFormatException) {
                 val msg = "Could not parse example ID: "
                 throw JsonParseException(msg + (e.message ?: "Unknown error"))
@@ -91,14 +90,26 @@ class StateKeeper {
         }
 
         private fun verifyMAC(payload: String, mac: String): Boolean {
-            val payloadWithKey = "$payload|$date|$key"
+            val payloadWithKey = "$payload|$date|${state.key}"
             val calculatedMAC = toHex(payloadWithKey.digestKeccak(parameter = KeccakParameter.SHA3_256))
             return calculatedMAC == mac.toUpperCase()
         }
 
         private fun toHex(bytes: ByteArray) = bytes.map { String.format("%02X", it) }.joinToString("")
+
+        private fun flush() {
+            val json = Json.stringify(AppState.serializer(), state)
+            storage.writeText(json)
+        }
     }
 }
+
+@Serializable
+data class AppState(
+    val enabledCalculi: MutableList<String> = mutableListOf(),
+    val examples: MutableList<Example> = mutableListOf(),
+    val key: String = "WildFlowers/UncomfortableMoons"
+)
 
 @Serializable
 data class Example(
