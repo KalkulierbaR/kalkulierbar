a8b43691c88c5d8129a8662c6e4a2830af62b82d
Mirko Hirsch <hirsch.mirko@gmail.com>
2020-03-15 20:02:25 +0100

    Close functionality

--- backend/src/main/kotlin/kalkulierbar/nonclausaltableaux/NonClausalTableauxMisc.kt
+++ backend/src/main/kotlin/kalkulierbar/nonclausaltableaux/NonClausalTableauxMisc.kt
@@ -1,6 +1,9 @@
 package kalkulierbar.nonclausaltableaux
 
+import kalkulierbar.InvalidFormulaFormat
+import kalkulierbar.logic.FirstOrderTerm
 import kalkulierbar.logic.LogicNode
+import kalkulierbar.parsers.FirstOrderParser
 import kalkulierbar.tamperprotect.ProtectedState
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -16,6 +19,22 @@ class NcTableauxState(
     var expansionCounter = 0
 
     override var seal = ""
+
+    /**
+     * Check whether a node is a (transitive) parent of another node
+     * @param parentID Node to check parenthood of
+     * @param childID Child node of suspected parent
+     * @return true iff the parentID is a true ancestor of the childID
+     */
+    @Suppress("ReturnCount")
+    fun nodeIsParentOf(parentID: Int, childID: Int): Boolean {
+        val child = nodes.get(childID)
+        if (child.parent == parentID)
+            return true
+        if (child.parent == 0 || child.parent == null)
+            return false
+        return nodeIsParentOf(parentID, child.parent!!)
+    }
 
     override fun getHash(): String {
         return "sphinxofblackquartz"
@@ -64,7 +83,24 @@ class CloseMove(
     val leafID: Int,
     val closeID: Int,
     val varAssign: Map<String, String>?
-) : NcTableauxMove()
+) : NcTableauxMove() {
+    /**
+     * Parses map values to first-order terms
+     * @return null iff varAssign == null
+     *         parsed map-values iff varAssign != null
+     */
+    fun getVarAssignTerms(): Map<String, FirstOrderTerm>? {
+        if (varAssign == null)
+            return null
+        return varAssign.mapValues {
+            try {
+                FirstOrderParser.parseTerm(it.value)
+            } catch (e: InvalidFormulaFormat) {
+                throw InvalidFormulaFormat("Could not parse term '${it.value}': ${e.message}")
+            }
+        }
+    }
+}
 
 @Serializable
 @SerialName("undo")
