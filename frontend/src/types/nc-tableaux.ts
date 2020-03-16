e268e2a17e68b02cbfc77b8556f61645efa4d315
drodt <d.drodt@gmx.de>
2020-03-16 18:27:57 +0100

    Add TS logic node types

--- frontend/src/types/nc-tableaux.ts
+++ frontend/src/types/nc-tableaux.ts
@@ -9,7 +9,80 @@ export interface NCTableauxState {
     seal: string;
 }
 
-export interface LogicNode {}
+export interface LogicVar {
+    type: "var";
+    spelling: string;
+}
+
+export interface LogicNot {
+    type: "not";
+    child: LogicNode;
+}
+
+export interface BinaryLogicNode {
+    type: string;
+    leftChild: LogicNode;
+    rightChild: LogicNode;
+}
+
+export interface LogicAnd extends BinaryLogicNode {
+    type: "and";
+}
+
+export interface LogicOr extends BinaryLogicNode {
+    type: "or";
+}
+
+export interface LogicImpl extends BinaryLogicNode {
+    type: "impl";
+}
+
+export interface LogicRelation {
+    spelling: string;
+    arguments: FOTerm[];
+}
+
+export interface Quantifier {
+    varName: string;
+    child: LogicNode;
+    boundVariables: QuantifiedVar[];
+}
+
+export interface AllQuantifier extends Quantifier {
+    type: "allquant";
+}
+
+export interface ExQuantifier extends Quantifier {
+    type: "exquant";
+}
+
+export type LogicNode =
+    | LogicVar
+    | LogicNot
+    | LogicAnd
+    | LogicOr
+    | LogicImpl
+    | LogicRelation
+    | AllQuantifier
+    | ExQuantifier;
+
+export interface QuantifiedVar {
+    type: "QuantifiedVariable";
+    spelling: string;
+}
+
+export interface Constant {
+    type: "Constant";
+    spelling: string;
+}
+
+export interface FOFunction {
+    type: "Function";
+    spelling: string;
+    arguments: FOTerm[];
+}
+
+export type FOTerm = QuantifiedVar | Constant | FOFunction;
 
 export interface NCTableauxNode {
     parent: number | null;
