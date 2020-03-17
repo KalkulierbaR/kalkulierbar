d235f2244b8fdc41f93e8e60aa7c7bb083bacddb
rec0de <mail@rec0de.net>
2020-03-17 23:21:15 +0100

    Add nc-tableaux tests

---
+++ backend/src/test/kotlin/kalkulierbar/nonclausaltableaux/TestGamma.kt
@@ -0,0 +1,81 @@
+package kalkulierbar.nonclausaltableaux
+
+import kalkulierbar.IllegalMove
+import kalkulierbar.parsers.FirstOrderParser
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TestGamma {
+
+    val instance = NonClausalTableaux()
+
+    @Test
+    fun testInvalidFormula() {
+        val formula = FirstOrderParser.parse("/ex X: P(X)")
+        val state = NcTableauxState(formula)
+
+        assertFailsWith<IllegalMove> {
+            instance.applyMoveOnState(state, GammaMove(0))
+        }
+    }
+
+    @Test
+    fun testInvalidIndex() {
+        val formula = FirstOrderParser.parse("/all X: P(X)")
+        val state = NcTableauxState(formula)
+
+        assertFailsWith<IllegalMove> {
+            instance.applyMoveOnState(state, GammaMove(1))
+        }
+
+        assertFailsWith<IllegalMove> {
+            instance.applyMoveOnState(state, GammaMove(-1))
+        }
+    }
+
+    @Test
+    fun testBasic() {
+        val formula = FirstOrderParser.parse("/all X: P(X)")
+        var state = NcTableauxState(formula)
+
+        state = instance.applyMoveOnState(state, GammaMove(0))
+
+        assertEquals("P(X_1)", state.nodes[1].spelling)
+        assertEquals(0, state.nodes[1].parent)
+        assertEquals(2, state.nodes.size)
+    }
+
+    @Test
+    fun testScoping() {
+        val formula = FirstOrderParser.parse("/all X: (P(X, f(X, g(c, X))) & Q(X) & /all X: C(X))")
+        var state = NcTableauxState(formula)
+
+        state = instance.applyMoveOnState(state, GammaMove(0))
+
+        assertEquals("((P(X_1, f(X_1, g(c, X_1))) ∧ Q(X_1)) ∧ (∀X: C(X)))", state.nodes[1].spelling)
+        assertEquals(0, state.nodes[1].parent)
+        assertEquals(2, state.nodes.size)
+    }
+
+    @Test
+    fun testSuffixCounter() {
+        val formula = FirstOrderParser.parse("/all X: /all Y: /all Z: R(X, Y, Z)")
+        var state = NcTableauxState(formula)
+
+        state = instance.applyMoveOnState(state, GammaMove(0))
+
+        assertEquals("(∀Y: (∀Z: R(X_1, Y, Z)))", state.nodes[1].spelling)
+        assertEquals(0, state.nodes[1].parent)
+
+        state = instance.applyMoveOnState(state, GammaMove(1))
+
+        assertEquals("(∀Z: R(X_1, Y_2, Z))", state.nodes[2].spelling)
+        assertEquals(1, state.nodes[2].parent)
+
+        state = instance.applyMoveOnState(state, GammaMove(2))
+
+        assertEquals("R(X_1, Y_2, Z_3)", state.nodes[3].spelling)
+        assertEquals(2, state.nodes[3].parent)
+    }
+}
