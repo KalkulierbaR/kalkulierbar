2933d7c72f51f293b2b7413deedecab20852588d
drodt <d.drodt@gmx.de>
2020-03-17 15:05:56 +0100

    Handle example state

--- frontend/src/routes/nc-tableaux/view/index.tsx
+++ frontend/src/routes/nc-tableaux/view/index.tsx
@@ -1,6 +1,17 @@
 import { h } from "preact";
+import { useAppState } from "../../../util/app-state";
+import exampleState from "./example";
 
 const NCTableauxView: preact.FunctionalComponent = () => {
+    const { "nc-tableaux": cState, onChange } = useAppState();
+
+    let state = cState;
+
+    if (!state) {
+        state = exampleState;
+        onChange("nc-tableaux", state);
+    }
+
     return <div></div>;
 };
 
