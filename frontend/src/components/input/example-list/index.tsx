19b8ffbfd7bd6dd5de6a9efa0b5af5bac07a0af4
drodt <d.drodt@gmx.de>
2020-03-19 17:23:16 +0100

    Hide example list when empty

--- frontend/src/components/input/example-list/index.tsx
+++ frontend/src/components/input/example-list/index.tsx
@@ -1,6 +1,6 @@
-import {h} from "preact";
-import {CalculusType} from "../../../types/app";
-import {delExample} from "../../../util/api";
+import { h } from "preact";
+import { CalculusType } from "../../../types/app";
+import { delExample } from "../../../util/api";
 import { useAppState } from "../../../util/app-state";
 import Btn from "../../btn";
 
@@ -16,14 +16,8 @@ interface Props {
 }
 
 const onDelete = (index: number) => {
-    const { server, onError, adminKey, setConfig} = useAppState();
-    delExample(
-        server,
-        index,
-        adminKey,
-        setConfig,
-        onError,
-    );
+    const { server, onError, adminKey, setConfig } = useAppState();
+    delExample(server, index, adminKey, setConfig, onError);
 };
 
 const ExampleList: preact.FunctionalComponent<Props> = ({
@@ -31,23 +25,27 @@ const ExampleList: preact.FunctionalComponent<Props> = ({
     className,
 }) => {
     const { config } = useAppState();
-    const examples = config.examples.filter(exmpl => exmpl.calculus === calculus);
+    const examples = config.examples.filter(
+        (example) => example.calculus === calculus,
+    );
 
-    return(
+    if (!examples.length) {
+        return null;
+    }
+
+    return (
         <div class={`card ${className}`}>
-            {examples.map( (exmpl, index) => (
+            {examples.map((example, index) => (
                 <div class="card">
-                    <p>{exmpl.name}</p>
-                    <p>{exmpl.description}</p>
-                    <p>{exmpl.formula}</p>
-                    <p>{exmpl.params}</p>
-                    <Btn onClick={() => onDelete(index)}>
-                        Delete
-                    </Btn>
+                    <p>{example.name}</p>
+                    <p>{example.description}</p>
+                    <p>{example.formula}</p>
+                    <p>{example.params}</p>
+                    <Btn onClick={() => onDelete(index)}>Delete</Btn>
                 </div>
             ))}
         </div>
-    )
+    );
 };
 
 export default ExampleList;
