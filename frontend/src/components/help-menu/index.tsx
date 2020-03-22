c69195447da486db764fd895ef58867bb34864e2
drodt <d.drodt@gmx.de>
2020-03-22 13:08:05 +0100

    Optimize tutorial videos

--- frontend/src/components/help-menu/index.tsx
+++ frontend/src/components/help-menu/index.tsx
@@ -1,7 +1,7 @@
-import {Fragment, h} from "preact";
-import {useState} from "preact/hooks";
-import {CalculusType, TableauxCalculus, TutorialMode} from "../../types/app";
-import {useAppState} from "../../util/app-state";
+import { Fragment, h } from "preact";
+import { useState } from "preact/hooks";
+import { CalculusType, TableauxCalculus, TutorialMode } from "../../types/app";
+import { useAppState } from "../../util/app-state";
 import Btn from "../btn";
 import Dialog from "../dialog";
 import FAB from "../fab";
@@ -12,12 +12,12 @@ interface Props {
     calculus: CalculusType;
 }
 
-const HelpMenu: preact.FunctionalComponent<Props> = ({
-    calculus,
-}) => {
+const HelpMenu: preact.FunctionalComponent<Props> = ({ calculus }) => {
     const { tutorialMode } = useAppState();
-    const [showDialog, setShowDialog] = useState(tutorialMode  !== TutorialMode.None);
-    
+    const [showDialog, setShowDialog] = useState(
+        tutorialMode !== TutorialMode.None,
+    );
+
     return (
         <Fragment>
             <FAB
@@ -34,25 +34,41 @@ const HelpMenu: preact.FunctionalComponent<Props> = ({
                 label="Tutorial"
                 onClose={() => setShowDialog(false)}
             >
-                {TableauxCalculus.includes(calculus) &&
+                {TableauxCalculus.includes(calculus) && (
                     <div class="flex-container">
                         <div class={`first  ${style.firstSpace}`}>
                             <h3>Expand move</h3>
-                            <img src="../../assets/gifs/prop_tableaux_expand.gif" alt="" />
-                            <p>You can expand the tree by choosing a leaf and a clause.</p>
+                            <video autoPlay loop alt="Tableaux Expand Move">
+                                <source
+                                    src="../../assets/gifs/prop_tableaux_expand.mp4"
+                                    type="video/mp4"
+                                />
+                            </video>
+                            <p>
+                                You can expand the tree by choosing a leaf and a
+                                clause.
+                            </p>
                         </div>
                         <div class="second">
                             <h3>Close move</h3>
-                            <img src="../../assets/gifs/prop_tableaux_close.gif" alt="" />
+                            <video autoPlay loop alt="Tableaux Close Move">
+                                <source
+                                    src="../../assets/gifs/prop_tableaux_close.mp4"
+                                    type="video/mp4"
+                                />
+                            </video>
                             <p>
-                                Close a leaf with a complementary node on the path to the root.
-                                The proof is complete when all leafs are closed.
+                                Close a leaf with a complementary node on the
+                                path to the root. The proof is complete when all
+                                leafs are closed.
                             </p>
                         </div>
                     </div>
-                }
+                )}
                 <p>
-                    <Btn onClick={() => setShowDialog(false)}>Okay I got it</Btn>
+                    <Btn onClick={() => setShowDialog(false)}>
+                        Okay I got it
+                    </Btn>
                 </p>
             </Dialog>
         </Fragment>
