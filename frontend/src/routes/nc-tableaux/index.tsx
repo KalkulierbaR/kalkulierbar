99f8688ba98020564d10d0f668b362727ae1ac21
Julius Henke <juliusb.henke@gmail.com>
2020-03-26 22:34:28 +0100

    Add upload button

--- frontend/src/routes/nc-tableaux/index.tsx
+++ frontend/src/routes/nc-tableaux/index.tsx
@@ -1,4 +1,5 @@
 import { Fragment, h } from "preact";
+import UploadFAB from "../../components/btn/upload";
 import FormulaInput from "../../components/input/formula";
 import Format from "../../components/input/formula/format";
 import { Calculus } from "../../types/app";
@@ -7,7 +8,12 @@ const NCTableaux: preact.FunctionalComponent = () => {
     return (
         <Fragment>
             <Format foLogic={true} />
-            <FormulaInput calculus={Calculus.ncTableaux} params={null} />
+            <FormulaInput
+                calculus={Calculus.ncTableaux}
+                params={null}
+                foLogic={true}
+            />
+            <UploadFAB calculus={Calculus.ncTableaux} />
         </Fragment>
     );
 };
