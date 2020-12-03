8ee22235bb8089ce9cb22360ecd492998d249085
ashim.siwakoti <ashim.siwakoti@stud.tu-darmstadt.de>
2020-12-03 11:43:42 +0100

    PSC parser that returns the clause set, (route -> psc/parse/)

--- backend/src/main/kotlin/kalkulierbar/psc/PSC.kt
+++ backend/src/main/kotlin/kalkulierbar/psc/PSC.kt
@@ -4,40 +4,57 @@ import kalkulierbar.CloseMessage
 import kalkulierbar.IllegalMove
 import kalkulierbar.JSONCalculus
 import kalkulierbar.JsonParseException
+import kalkulierbar.psc.*
+import kalkulierbar.psc.NodeType
 import kalkulierbar.parsers.FlexibleClauseSetParser
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.plus
-import kotlinx.serialization.Serializable
 
 class PSC : JSONCalculus<PSCState, PSCMove, Unit>() {
 
-    override val identifier = "psc";
+    override val identifier = "psc"
 
-    private val serializer = Json(context = pscMoveModule)
+    private val serializer = Json(context = pscMoveModule + kalkulierbar.psc.clausesetDiffModule)
 
 
     override fun parseFormulaToState(formula: String, params: Unit?): PSCState {
-        // val clauses = FlexibleClauseSetParser.parse(formula)
-        // val state = DPLLState(clauses)
-        // state.tree.add(TreeNode(null, NodeType.ROOT, "true", Identity()))
-
-        val state = PSCState(formula);
-
+        val clauses = FlexibleClauseSetParser.parse(formula)
+        val state = PSCState(clauses);
+        state.tree.add(TreeNode(null, NodeType.ROOT, "true", Identity()))
+        print(state)
         return state
     }
 
     override fun applyMoveOnState(state: PSCState, move: PSCMove): PSCState {
-        when (move) {
-            // is MoveNotLeft -> notLeft(state: PSCState, move: PSCMove);
-            else -> throw IllegalMove("Unknown move")
-        }
+//        when (move) {
+//            is MovePropagate -> kalkulierbar.psc.propagate(state, move.branch, move.baseClause, move.propClause, move.propAtom)
+//            is MoveSplit -> kalkulierbar.psc.split(state, move.branch, move.literal)
+//            is MovePrune -> kalkulierbar.psc.prune(state, move.branch)
+//            is MoveModelCheck -> kalkulierbar.psc.checkModel(state, move.branch, move.interpretation)
+//            else -> throw IllegalMove("Unknown move")
+//        }
         return state
     }
 
     // override fun checkClose(state: String) = closeToJson(checkCloseOnState(jsonToState(state)))
 
     override fun checkCloseOnState(state: PSCState): CloseMessage {
-        return CloseMessage(false, "Proof is not closed")
+        // A proof is closed if every leaf is a 'closed' annotation
+        // (which means every proper leaf contains the empty clause)
+        val closed = state.tree.all { !it.isLeaf || it.type == NodeType.CLOSED }
+        // There are no more rules to be applied if every leaf is an annotation
+        // (-> every proper leaf is either closed or has a model)
+        val done = state.tree.all { !it.isLeaf || it.isAnnotation }
+        val msg: String
+
+        if (closed)
+            msg = "The proof is closed and proves the unsatisfiability of the clause set"
+        else {
+            val donemsg = if (done) "- however, all branches are completed. The clause set is satisfiable." else "yet."
+            msg = "The proof is not closed $donemsg"
+        }
+
+        return CloseMessage(closed, msg)
     }
 
     @Suppress("TooGenericExceptionCaught")
@@ -46,6 +63,9 @@ class PSC : JSONCalculus<PSCState, PSCMove, Unit>() {
             val parsed = serializer.parse(PSCState.serializer(), json)
 
             // Ensure valid, unmodified state object
+            if (!parsed.verifySeal())
+                throw JsonParseException("Invalid tamper protection seal, state object appears to have been modified")
+
             return parsed
         } catch (e: Exception) {
             val msg = "Could not parse JSON state: "
@@ -54,6 +74,7 @@ class PSC : JSONCalculus<PSCState, PSCMove, Unit>() {
     }
 
     override fun stateToJson(state: PSCState): String {
+        state.computeSeal()
         return serializer.stringify(PSCState.serializer(), state)
     }
 
--- backend/src/main/kotlin/kalkulierbar/psc/PSCmisc.kt
+++ backend/src/main/kotlin/kalkulierbar/psc/PSCmisc.kt
@@ -1,53 +1,114 @@
 package kalkulierbar.psc
 
-import kalkulierbar.CloseMessage
-import kalkulierbar.IllegalMove
-import kalkulierbar.JSONCalculus
-import kalkulierbar.JsonParseException
-import kalkulierbar.parsers.FlexibleClauseSetParser
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.plus
+import kalkulierbar.clause.ClauseSet
+import kalkulierbar.tamperprotect.ProtectedState
 import kotlinx.serialization.Serializable
 
-
 @Serializable
-class PSCState(val formula: String) {
-    val root: TreeNode = TreeNode(null, formula);
-}
+class PSCState(val clauseSet: ClauseSet<String>) : ProtectedState() {
+    val tree = mutableListOf<TreeNode>()
 
-@Serializable
-class TreeNode() {
-    
-    var parent: TreeNode? = null;
-    var leftChild: TreeNode? = null;
-    var rightChild: TreeNode? = null;
-    var leftExpression: Expression? = null;
-    var rightExpression: Expression? = null;
-    
-    constructor(parent: TreeNode?, label: String): this() {
-        this.parent = parent;
+    /**
+     * Remove all children of a node from the proof tree
+     * This requires some index shifting magic due to the list representation
+     * of the tree, but I figure it's still better than figuring out a way to
+     * serialize doubly-linked trees and define IDs on that
+     * @param id ID of the node whose children are to be pruned
+     */
+    fun pruneBranch(id: Int) {
+        // Collect all transitive children of the node
+        // (not deleting anything yet to keep index structures intact)
+        val queue = mutableListOf<Int>()
+        val toDelete = mutableListOf<Int>()
+        queue.addAll(tree[id].children)
+
+        while (queue.isNotEmpty()) {
+            val index = queue.removeAt(0)
+            val node = tree[index]
+            queue.addAll(node.children)
+            toDelete.add(index)
+        }
+
+        // Remove each identified child, keeping parent references but not children references
+        // We remove items from the largest index to the smallest to keep the indices of the other
+        // items in the list consistent
+        toDelete.sorted().asReversed().forEach {
+            removeNodeInconsistent(it)
+        }
+
+        // Re-compute children references
+        rebuildChildRefs()
     }
 
-    constructor(parent: TreeNode?, exprLeft: Expression, exprRight: Expression): this() {
-        this.parent = parent;
-        this.leftExpression = exprLeft;
-        this.rightExpression = exprRight
+    /**
+     * Removes a node from the proof tree, keeping parent references intact
+     * NOTE: This will most likely leave the children references in an INCONSISTENT state
+     *       Use rebuildChildRefs() to ensure valid children references
+     * @param id ID of the node to remove
+     */
+    private fun removeNodeInconsistent(id: Int) {
+        tree.removeAt(id)
+        tree.forEach {
+            if (it.parent != null && it.parent!! > id)
+                it.parent = it.parent!! - 1
+        }
     }
 
+    /**
+     * Rebuilds children references in the entire proof tree from parent references
+     */
+    private fun rebuildChildRefs() {
+        tree.forEach { it.children.clear() }
+
+        for (i in tree.indices) {
+            if (tree[i].parent != null)
+                tree[tree[i].parent!!].children.add(i)
+        }
+    }
+
+    /**
+     * Applies the clause set deltas stored in the proof tree to 'check out'
+     * the full clause set of a node in the tree
+     * @param branch ID of the node/branch whose clause set should be computed
+     * @return Full clause set associated with the given node
+     */
+    fun getClauseSet(branch: Int): ClauseSet<String> {
+        var node = tree[branch]
+        val diffs = mutableListOf<CsDiff>()
+        var res = clauseSet
+
+        while (node.parent != null) {
+            diffs.add(node.diff)
+            node = tree[node.parent!!]
+        }
+
+        diffs.asReversed().forEach {
+            res = it.apply(res)
+        }
+
+        return res
+    }
+
+    override var seal = ""
+    override fun getHash() = "ppsc|$clauseSet|${tree.map{it.getHash()}}"
 }
 
 @Serializable
-class atom(val label: String, val booleanValue: Boolean) : Expression(null, null, null) {
+class TreeNode(var parent: Int?, val type: NodeType, var label: String, val diff: CsDiff) {
+    val children = mutableListOf<Int>()
+    val isLeaf
+        get() = children.size == 0
+    val isAnnotation
+        get() = (type == NodeType.MODEL || type == NodeType.CLOSED)
+    var modelVerified: Boolean? = null
 
+    fun getHash(): String {
+        return "($parent|$children|$type|$label|$diff|$modelVerified)"
+    }
+
+    override fun toString() = label
 }
 
-@Serializable
-open class Expression(val operation: ExpressionOperation?, val op1: Expression?, val op2: Expression?) {
-
-}
-
-enum class ExpressionOperation() {
-    NOT,
-    OR,
-    AND,
+enum class NodeType {
+    ROOT, PROP, SPLIT, MODEL, CLOSED
 }
