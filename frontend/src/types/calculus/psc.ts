883ad9374381a1e07efd0f7e611a4fde39686541
Rico3110 <Rico3110@users.noreply.github.com>
2020-12-11 19:11:23 +0100

    PSC View Site
    app.tsx: fix Path issues
    routes/psc/view: added index.tsx
    routes/psc/view: added style.scss
    types/app/app-state: added PSCState
    types/calculus: added psc.ts
    types/calculus: added rules.ts

---
+++ frontend/src/types/calculus/psc.ts
@@ -0,0 +1,28 @@
+import { RuleSet } from "./rules";
+import { PropCalculusType, Calculus } from ".";
+
+export interface PSCNode {
+    parent: number | null;
+    leftChild: number | null;
+    rightChild: number | null;
+    leftClauses: string[];
+    rightClause: string[];
+}
+
+export type PSCTreeLayoutNode = PSCNode & { id: number};
+
+export interface PSCState {
+    ruleSet: RuleSet;
+    nodes: PSCNode[];
+}
+
+export function instanceOfPSCState(
+    object: any,
+    calculus: PropCalculusType,
+): object is PSCState {
+    return "ruleSet" in object && calculus === Calculus.psc;
+}
+
+export interface PSCMove {
+    type: "standard"
+}
