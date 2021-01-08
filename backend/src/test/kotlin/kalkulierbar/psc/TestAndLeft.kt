819485fff6b848a616a713fe559dc79f33d1cbc5
psychoashim <ashim.siwakoti@stud.tu-darmstadt.de>
2021-01-08 19:49:53 +0100

    " Test cases for AndLeft, OrRight, NotLeft and NotRight. Test notLeft not fully implemented "

--- backend/src/test/kotlin/kalkulierbar/psc/TestAndLeft.kt
+++ backend/src/test/kotlin/kalkulierbar/psc/TestAndLeft.kt
@@ -1,12 +1,54 @@
 package kalkulierbar.psc
 
+import kalkulierbar.IllegalMove
+import kalkulierbar.logic.Var
+import kalkulierbar.parsers.PropositionalParser
 import kalkulierbar.psc.PSC
 import kalkulierbar.psc.PSCMove
 import kalkulierbar.psc.PSCState
-import kotlin.test.assertEquals
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
+import kotlin.test.*
 
 class TestAndLeft {
+    val instance = PSC()
+    val parser = PropositionalParser()
+
+    @Test
+    fun testBasic() {
+        var state = instance.parseFormulaToState("!(a & b)", null)
+
+        state = instance.applyMoveOnState(state, NotRight(0,0))
+        state = instance.applyMoveOnState(state, AndLeft(1,0))
+
+        val formula1 = parser.parse("a ")
+        val formula2 = parser.parse("b ")
+        val node1 = state.tree[state.tree.size - 1]
+
+
+
+        assertTrue(node1.leftFormula[0].synEq(formula1))
+        assertTrue(node1.leftFormula[1].synEq(formula2))
+
+        assertTrue(node1.leftFormula[0] is Var)
+        assertTrue(node1.leftFormula[1] is Var)
+
+    }
+    fun testParent(){
+        var state = instance.parseFormulaToState("!(a | b)", null)
+        state = instance.applyMoveOnState(state, NotRight(0,0))
+        assertTrue(state.tree[0].parent == null)
+
+        state = instance.applyMoveOnState(state, OrRight(0,0))
+
+        assertTrue(state.tree[0] is OneChildNode)
+        assertEquals(state.tree[1].parent, 0)
+    }
+
+
+    fun testWrongNode() {
+        var state = instance.parseFormulaToState("a & b", null)
+
+        assertFailsWith<IllegalMove> {
+            instance.applyMoveOnState(state, AndLeft(0,0))
+        }
+    }
 }
--- backend/src/test/kotlin/kalkulierbar/psc/TestNotRight.kt
+++ backend/src/test/kotlin/kalkulierbar/psc/TestNotRight.kt
@@ -1,14 +1,47 @@
 package kalkulierbar.psc
 
+import kalkulierbar.IllegalMove
+import kalkulierbar.logic.Var
+import kalkulierbar.parsers.PropositionalParser
 import kalkulierbar.psc.PSC
 import kalkulierbar.psc.PSCMove
 import kalkulierbar.psc.PSCState
-import kotlin.test.assertEquals
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
+import kotlin.test.*
 
 class TestNotRight {
+    val instance = PSC()
+    val parser = PropositionalParser()
 
+    @Test
+    fun testBasic(){
+        var state = instance.parseFormulaToState("!a", null)
 
+        state = instance.applyMoveOnState(state, NotRight(0,0))
+        val formula1 = parser.parse("a ")
+        val node1 = state.tree[state.tree.size - 1]
+
+        assertTrue(node1.leftFormula[0].synEq(formula1))
+
+        assertTrue(node1.leftFormula[0] is Var)
+    }
+
+    @Test
+    fun testParent(){
+        var state = instance.parseFormulaToState("!a ", null)
+        assertTrue(state.tree[0].parent == null)
+
+        state = instance.applyMoveOnState(state, NotRight(0,0))
+
+        assertTrue(state.tree[0] is OneChildNode)
+        assertEquals(state.tree[1].parent, 0)
+    }
+
+    @Test
+    fun testWrongNode() {
+        var state = instance.parseFormulaToState("a & !a", null)
+
+        assertFailsWith<IllegalMove> {
+            instance.applyMoveOnState(state, NotRight(0,0))
+        }
+    }
 }
